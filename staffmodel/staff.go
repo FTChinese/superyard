ee4576eb1de86a84c8fb04c1b0319ef70bc478b1
weiguo.ni <neefrankie@gmail.com>
2018-09-29 18:58:33 +0800

    data struct for new staff

---
+++ staffmodel/staff.go
@@ -0,0 +1,11 @@
+package staffmodel
+
+// Staff creates a new employee
+type Staff struct {
+	UserName     string `json:"userName"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	DisplayName  string `json:"displayName"`
+	Department   string `json:"department"`
+	GroupMembers int    `json:"groupMembers"`
+}
