c8b4c0a41fdf59bd2174e265e13555292db659e2
weiguo.ni <neefrankie@gmail.com>
2018-10-01 14:19:58 +0800

    rm Email filed

--- staff/password_reset.go
+++ staff/password_reset.go
@@ -10,16 +10,37 @@ const resetLetterURL = "http://localhost:8001/backyard/password-reset"
 // PasswordReset is used as marshal target when user tries to reset password via email
 type PasswordReset struct {
 	Token    string `json:"token"`
-	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+// LetterAddress finds a user's name, email and display name by verifying its email.
+func (env Env) LetterAddress(email string) (LetterAddress, error) {
+	query := `
+	SELECT username AS userName,
+		email,
+		display_name AS displayName
+	FROM backyard.staff
+	WHERE email = ?
+	LIMIT 1`
+
+	var a LetterAddress
+	err := env.DB.QueryRow(query, email).Scan(
+		&a.UserName,
+		&a.Email,
+		&a.DisplayName,
+	)
+
+	if err != nil {
+		staffLogger.WithField("location", "Verify password reset email").Error(err)
+
+		return a, err
+	}
+
+	return a, nil
+}
+
 // CreateResetToken send a password reset token to a user's email
-// Workflow:
-// 1. When request comes, get email from request;
-// 2. Use email to find a user's account. 404 if not found;
-// 3. Call this function to create a reset token adn send a leeter.CreateResetToken
-func (env Env) CreateResetToken(a Account) error {
+func (env Env) CreateResetToken(a LetterAddress) error {
 	token, err := util.RandomHex(32)
 
 	staffLogger.Infof("Password reset token: %s\n", token)
@@ -63,25 +84,53 @@ func (env Env) CreateResetToken(a Account) error {
 	return nil
 }
 
-// EmailForReset finds the email associated with a password reset token
-func (env Env) EmailForReset(token string) (string, error) {
+// VerifyResetToken finds the account associated with a password reset token
+// If an account associated with a token if found, allow user to reset password.
+func (env Env) VerifyResetToken(token string) (LetterAddress, error) {
 	query := `
-	SELECT email AS email
-    FROM backyard.password_reset
+	SELECT s.username AS userName,
+		s.email,
+		s.display_name AS displayName,
+	FROM backyard.password_reset AS p
+		LEFT JOIN backyard.staff AS s
+		ON p.email = s.email
     WHERE token = UNHEX(?)
       AND DATE_ADD(created_utc, INTERVAL expires_in SECOND) > UTC_TIMESTAMP()
 	LIMIT 1`
 
-	var email string
-	err := env.DB.QueryRow(query, token).Scan(&email)
+	var a LetterAddress
+	err := env.DB.QueryRow(query, token).Scan(
+		&a.UserName,
+		&a.Email,
+		&a.DisplayName,
+	)
 
 	if err != nil {
 		staffLogger.WithField("location", "Find email associated with a reset token").Error(err)
 
-		return email, err
+		return a, err
 	}
 
-	return email, nil
+	return a, nil
+}
+
+// ResetPassword allows user to reset password after clicked the password reset link in its email.
+func (env Env) ResetPassword(r PasswordReset) error {
+	// First check if the token is associated with an account
+	addr, err := env.VerifyResetToken(r.Token)
+
+	if err != nil {
+		return err
+	}
+
+	// The account associated with a token is found. Chnage password.
+	err = env.changePassword(addr.UserName, r.Password)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // DeleteResetToken deletes a password reset token after it is used.
