4b8ae97b27092f4a024454e1b43ff243d7dd2b18
weiguo.ni <neefrankie@gmail.com>
2018-10-03 19:08:53 +0800

    split into defferent files for easy reading

--- ftcapi/api_key.go
+++ ftcapi/api_key.go
@@ -1,5 +1,12 @@
 package ftcapi
 
+import (
+	"fmt"
+	"strings"
+
+	"gitlab.com/ftchinese/backyard-api/util"
+)
+
 // APIKey is an OAuth 2.0 access token used by an app or person to access ftc api
 type APIKey struct {
 	ID          int    `json:"id"`
@@ -11,4 +18,163 @@ type APIKey struct {
 	LastUsed    string `json:"lastUsed"`
 	CreatedBy   string `json:"createdBy"`
 	OwnedByApp  string `json:"ownedByApp"`
+}
+
+// Sanitize removes leading and trailing spaces
+func (k *APIKey) Sanitize() {
+	k.Description = strings.TrimSpace(k.Description)
+	k.MyftID = strings.TrimSpace(k.MyftID)
+	k.OwnedByApp = strings.TrimSpace(k.OwnedByApp)
+}
+
+// NewAPIKey creates a new row in oauth.api_key table
+func (env Env) NewAPIKey(key APIKey) error {
+
+	token, err := util.RandomHex(20)
+
+	if err != nil {
+		logger.WithField("location", "Generating access token")
+		return err
+	}
+
+	query := `
+	INSERT INTO oauth.api_key
+    SET access_token = UNHEX(?),
+      	description = ?,
+      	myft_id = NULLIF(?, ''),
+		created_by = NULLIF(?, ''),
+		owned_by_app = NULLIF(?, '')`
+
+	_, err = env.DB.Exec(query,
+		token,
+		key.Description,
+		key.MyftID,
+		key.CreatedBy,
+		key.OwnedByApp,
+	)
+
+	if err != nil {
+		logger.WithField("location", "Create new ftc api key").Error(err)
+
+		return err
+	}
+
+	return nil
+}
+
+// apiKeyRoster show all api keys owned by a user or an app
+func (env Env) apiKeyRoster(w whereClause, value string) ([]APIKey, error) {
+	query := fmt.Sprintf(`
+	SELECT id AS id,
+		access_token AS token,
+		description AS description,
+		myft_id AS myftId,
+		created_utc AS createdAt,
+		updated_utc AS updatedAt,
+		last_used_utc AS lastUsedAt
+	FROM oauth.api_key
+	WHERE %s
+		AND is_active = 1
+	ORDER BY created_utc DESC`, w.String())
+
+	rows, err := env.DB.Query(query, value)
+
+	var keys []APIKey
+
+	if err != nil {
+		logger.WithField("location", "Retrieve api keys owned by a user").Error(err)
+
+		return keys, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var key APIKey
+
+		err := rows.Scan(
+			&key.ID,
+			&key.Token,
+			&key.Description,
+			&key.MyftID,
+			&key.CreateAt,
+			&key.UpdatedAt,
+			&key.LastUsed,
+		)
+
+		if err != nil {
+			logger.WithField("location", "Scan personal api key").Error(err)
+
+			continue
+		}
+
+		keys = append(keys, key)
+	}
+
+	if err := rows.Err(); err != nil {
+		logger.WithField("location", "Retrieve personal api keys iteration").Error(err)
+
+		return keys, err
+	}
+
+	return keys, nil
+}
+
+// PersonalAPIKeys lists all personal access tokens owned by a user.
+// This version no longer show individual token separately.
+func (env Env) PersonalAPIKeys(userName string) ([]APIKey, error) {
+	return env.apiKeyRoster(personalAccess, userName)
+}
+
+// AppAPIKeys show all access tokens owned by an app
+func (env Env) AppAPIKeys(appSlug string) ([]APIKey, error) {
+	return env.apiKeyRoster(appAccess, appSlug)
+}
+
+// Remove api key(s) owned by a person or an app.
+// w determines personal key or app's key;
+// id determined remove a specific key or all key owned by owner. 0 to remove all; other integer value specifies the key's id.
+func (env Env) deleteAPIAccess(w whereClause, id int, owner string) error {
+
+	var whereID string
+
+	if id > 0 {
+		whereID = "AND id = ?"
+	}
+	query := fmt.Sprintf(`
+	UPDATE oauth.api_key
+      SET is_active = 0
+    WHERE %s
+	  %s
+	LIMIT 1`, w.String(), whereID)
+
+	var err error
+
+	if id > 0 {
+		_, err = env.DB.Exec(query, owner, id)
+	} else {
+		_, err = env.DB.Exec(query, owner)
+	}
+
+	if err != nil {
+		logger.WithField("location", "Remove personal api key").Error(err)
+
+		return err
+	}
+
+	return nil
+}
+
+// RemovePersonalAccess removes one or all access token owned by a user.
+// id == 0 removes all owned by userName;
+// id > 0 removes only the one with this id.
+// NOTE: SQL's auto increment key starts from 1.
+func (env Env) RemovePersonalAccess(id int, userName string) error {
+	return env.deleteAPIAccess(personalAccess, id, userName)
+}
+
+// RemoveAppAccess removes one or all access token owned by an app.
+// id == 0 removes all owned by this app;
+// id > 0 removes only the one with the specified id.
+func (env Env) RemoveAppAccess(id int, appSlug string) error {
+	return env.deleteAPIAccess(appAccess, id, appSlug)
 }
--- ftcapi/app.go
+++ ftcapi/app.go
@@ -1,5 +1,12 @@
 package ftcapi
 
+import (
+	"fmt"
+	"strings"
+
+	"gitlab.com/ftchinese/backyard-api/util"
+)
+
 // App represents an application that needs to access ftc api
 type App struct {
 	ID           int    `json:"id"`
@@ -16,9 +23,233 @@ type App struct {
 	OwnedBy      string `json:"ownedBy"`
 }
 
+// Sanitize removes leading and trailing spaces
+func (a *App) Sanitize() {
+	a.Name = strings.TrimSpace(a.Name)
+	a.Slug = strings.TrimSpace(a.Slug)
+	a.RepoURL = strings.TrimSpace(a.RepoURL)
+	a.Description = strings.TrimSpace(a.Description)
+	a.HomeURL = strings.TrimSpace(a.HomeURL)
+}
+
 // Ownership is used to transfer an app's ownership
 type Ownership struct {
-	ID       int    `json:"id"`
-	NewOwner string `json:"newOwner"`
-	OldOwner string `json:"oldOwner"`
+	SlugName string
+	NewOwner string
+	OldOwner string
+}
+
+// NewApp inserts a new row into oauth.app_registry table
+func (env Env) NewApp(app App) error {
+	clientID, err := util.RandomHex(10)
+	if err != nil {
+		logger.WithField("location", "Generating client id").WithError(err)
+		return err
+	}
+
+	clientSecret, err := util.RandomHex(32)
+	if err != nil {
+		logger.WithField("location", "Generating client secret").WithError(err)
+
+		return err
+	}
+	query := `
+	INSERT INTO oauth.app_registry
+	SET app_name = ?,
+		app_slug = ?,
+        client_id = UNHEX(?),
+        client_secret = UNHEX(?),
+        repo_url = ?,
+        description = NULLIF(?, ''),
+        homepage_url = NULLIF(?, ''),
+		owned_by = ?`
+
+	_, err = env.DB.Exec(query,
+		app.Name,
+		app.Slug,
+		clientID,
+		clientSecret,
+		app.RepoURL,
+		app.Description,
+		app.HomeURL,
+		app.OwnedBy,
+	)
+
+	if err != nil {
+		logger.WithField("location", "Create new ftc app").Error(err)
+
+		return err
+	}
+
+	return nil
+}
+
+// AppRoster retrieves all ftc app with pagination
+func (env Env) AppRoster(page uint, rowCount uint) ([]App, error) {
+	offset := (page - 1) * rowCount
+
+	query := fmt.Sprintf(`
+	%s
+	ORDER BY created_utc DESC
+	LIMIT ? OFFSET ?`, stmtFTCApp)
+
+	rows, err := env.DB.Query(query, rowCount, offset)
+
+	var apps []App
+
+	if err != nil {
+		logger.WithField("location", "Retrieve all ftc apps").Error(err)
+
+		return apps, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var app App
+
+		err := rows.Scan(
+			&app.ID,
+			&app.Name,
+			&app.Slug,
+			&app.ClientID,
+			&app.ClientSecret,
+			&app.RepoURL,
+			&app.Description,
+			&app.HomeURL,
+			&app.IsActive,
+			&app.CreatedAt,
+			&app.UpdatedAt,
+			&app.OwnedBy,
+		)
+
+		if err != nil {
+			logger.WithField("location", "Scan a row when retriving all apps").Error(err)
+
+			continue
+		}
+
+		apps = append(apps, app)
+	}
+
+	if err := rows.Err(); err != nil {
+		logger.WithField("location", "Rows iteration when retriving all ftc apps").Error(err)
+
+		return apps, err
+	}
+
+	return apps, nil
+}
+
+// RetrieveApp retrieves a ftc app regardless of who owns it.
+// The whole team should be accessible to all apps.
+func (env Env) RetrieveApp(slugName string) (App, error) {
+	query := fmt.Sprintf(`
+	%s
+	WHERE app_slug = ?
+	LIMIT 1`, stmtFTCApp)
+
+	var app App
+	err := env.DB.QueryRow(query, slugName).Scan(
+		&app.ID,
+		&app.Name,
+		&app.Slug,
+		&app.ClientID,
+		&app.ClientSecret,
+		&app.RepoURL,
+		&app.Description,
+		&app.HomeURL,
+		&app.IsActive,
+		&app.CreatedAt,
+		&app.UpdatedAt,
+		&app.OwnedBy,
+	)
+
+	if err != nil {
+		logger.WithField("location", "Retrive one ftc app").Error(err)
+
+		return app, err
+	}
+
+	return app, nil
+}
+
+// UpdateApp allows user to update a ftc app.
+func (env Env) UpdateApp(slugName string, app App) error {
+	query := `
+	UPDATE oauth.app_registry
+	SET app_name = ?,
+	  	app_slug = ?
+        repo_url = ?,
+        description = IFNULL(?, description),
+        homepage_url = IFNULL(?, homepage_url),
+	WHERE slug_name = ?
+		AND owned_by = ?
+      	AND is_active = 1
+	LIMIT 1`
+
+	_, err := env.DB.Exec(query,
+		app.Name,
+		app.Slug,
+		app.RepoURL,
+		app.Description,
+		app.HomeURL,
+		slugName,
+		app.OwnedBy,
+	)
+
+	if err != nil {
+		logger.WithField("location", "Updating a ftc app").Error(err)
+
+		return err
+	}
+
+	return nil
+}
+
+// RemoveApp deactivate a ftc app
+func (env Env) RemoveApp(slugName, owner string) error {
+	query := `
+	UPDATE oauth.app_registry
+      	SET is_active = 0
+	WHERE app_slug = ?
+      	AND owned_by = ?
+      	AND is_active = 1
+	LIMIT 1`
+
+	_, err := env.DB.Exec(query, slugName, owner)
+
+	if err != nil {
+		logger.WithField("location", "Deactivate a ftc app").Error(err)
+
+		return err
+	}
+
+	return nil
+}
+
+// TransferApp tranfers ownership of an app
+// Before transfer, we must make sure the target owner actually exists.
+func (env Env) TransferApp(o Ownership) error {
+	query := `
+	UPDATE oauth.app_registry
+    	SET owned_by = ?
+	WHERE app_slug = ?
+		owned_by = ?
+      	AND is_active = 1
+	LIMIT 1`
+
+	_, err := env.DB.Exec(query,
+		o.NewOwner,
+		o.SlugName,
+		o.OldOwner,
+	)
+
+	if err != nil {
+		logger.WithField("location", "Transfer owership of a ftc app").Error(err)
+
+		return err
+	}
+
+	return nil
 }
