1a3bd7c6f42b7e66c5a25afac7cf38981698d582
weiguo.ni <neefrankie@gmail.com>
2018-10-04 16:45:33 +0800

    check username and parse form middlewares

--- controller/middleware.go
+++ controller/middleware.go
@@ -1,1 +1,51 @@
 package controller
+
+import (
+	"net/http"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"gitlab.com/ftchinese/backyard-api/util"
+	"gitlab.com/ftchinese/backyard-api/view"
+)
+
+const userNameKey = "X-User-Name"
+
+// CheckUserName makes sure the request header contains `X-User-Name` fields
+func CheckUserName(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		userName := req.Header.Get(userNameKey)
+
+		userName = strings.TrimSpace(userName)
+		if userName == "" {
+			log.WithField("location", "middleware: checkUserName").Info("Missing X-User-Name header")
+
+			view.Render(w, util.NewUnauthorized(""))
+
+			return
+		}
+
+		req.Header.Set(userNameKey, userName)
+
+		next.ServeHTTP(w, req)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+// ParseForm perform req.ParseForm and stops if any parse failed
+func ParseForm(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		err := req.ParseForm()
+
+		if err != nil {
+			view.Render(w, util.NewBadRequest(err.Error()))
+
+			return
+		}
+
+		next.ServeHTTP(w, req)
+	}
+
+	return http.HandlerFunc(fn)
+}
