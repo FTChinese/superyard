abf55a4de58d771b723070f0531dcd6875ea0fa4
weiguo.ni <neefrankie@gmail.com>
2018-10-04 15:07:32 +0800

    stats for daily new user

---
+++ stats/env.go
@@ -0,0 +1,68 @@
+package stats
+
+import (
+	"database/sql"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var logger = log.WithField("package", "ftcuser")
+
+// Env wraps db connection
+type Env struct {
+	DB *sql.DB
+}
+
+// Signup calculates how many new users signed up every day
+type Signup struct {
+	Count int    `json:"count"`
+	Date  string `json:"date"`
+}
+
+// DailyNewUser finds out how many new Singup everyday.
+// `start` and `end` are the time range to perform statistics.
+// Time format are `YYYY-MM-DD`
+func (env Env) DailyNewUser(start, end string) ([]Signup, error) {
+	query := `
+	SELECT COUNT(*) AS userCount,
+      DATE(register_time) AS recordDate
+    FROM cmstmp01.userinfo
+    WHERE DATE(register_time) BETWEEN DATE(?) AND DATE(?)
+    GROUP BY DATE(register_time)
+	ORDER BY DATE(register_time) DESC`
+
+	rows, err := env.DB.Query(query, start, end)
+
+	if err != nil {
+		logger.WithField("location", "Query daily new users").Error(err)
+
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var signups []Signup
+
+	for rows.Next() {
+		var s Signup
+		err := rows.Scan(
+			&s.Count,
+			&s.Date,
+		)
+
+		if err != nil {
+			logger.WithField("location", "Scan signup count")
+
+			continue
+		}
+
+		signups = append(signups, s)
+	}
+
+	if err := rows.Err(); err != nil {
+		logger.WithField("location", "Daily new user rows iteration").Error(err)
+		return nil, err
+	}
+
+	return signups, nil
+}
