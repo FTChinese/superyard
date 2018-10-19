92c7b4b9b706f665e26e8db2add170d769103e2e
weiguo.ni <neefrankie@gmail.com>
2018-10-20 06:17:37 +0800

    test already exists

--- util/errors.go
+++ util/errors.go
@@ -1,9 +1,27 @@
 package util
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/go-sql-driver/mysql"
+)
 
 // Define common errors
 var (
 	ErrForbidden     = errors.New("acess denied") // Tells controller what kind of  HTTP response to use
 	ErrWrongPassword = errors.New("wrong password")
+	ErrAlreadyExists = errors.New("already exists")
 )
+
+// IsAlreadyExists tests if an error means the field already exists
+func IsAlreadyExists(err error) bool {
+	if e, ok := err.(*mysql.MySQLError); ok && e.Number == 1062 {
+		return true
+	}
+
+	if err == ErrAlreadyExists {
+		return true
+	}
+
+	return false
+}
