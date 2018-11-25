b31b8966c9bf6f425a5b8c5df53b9c2110281f9e
weiguo.ni <neefrankie@gmail.com>
2018-11-25 23:01:31 +0800

    refactor

--- subscription/pricing.go
+++ subscription/pricing.go
@@ -1,11 +1,6 @@
 package subscription
 
-import (
-	"encoding/json"
-	"fmt"
-
-	"gitlab.com/ftchinese/backyard-api/util"
-)
+import "encoding/json"
 
 // Plan contains details of subscription plan.
 type Plan struct {
@@ -21,171 +16,25 @@ type Plan struct {
 	Message string `json:"message"`
 }
 
-// Schedule represents a discount activity
-type Schedule struct {
-	ID          int64           `json:"id"`
-	Name        string          `json:"name"`
-	Description string          `json:"description"`
-	Start       string          `json:"startAt"`
-	End         string          `json:"endAt"`
-	Plans       map[string]Plan `json:"plans"`
-	CreatedAt   string          `json:"createdAt"`
-	CreatedBy   string          `json:"createdBy"`
-}
-
-// NewShedule saves a new discount schedule into database.
-func (env Env) NewShedule(s Schedule) error {
+// NewPricing set the pricing plans of a promotion schedule.
+func (env Env) NewPricing(id int64, plans map[string]Plan) error {
 	query := `
-	INSERT INTO premium.discount_schedule
-	SET name = ?,
-		description = ?,
-		start_utc = ?,
-		end_utc = ?,
-		plans = ?,
-		created_utc = UTC_TIMESTAMP(),
-		created_by = ?`
-
-	startUTC := util.SQLDatetimeUTC.FromISO8601(s.Start)
-	endUTC := util.SQLDatetimeUTC.FromISO8601(s.End)
-	plans, err := json.Marshal(s.Plans)
-
-	if err != nil {
-		logger.WithField("location", "NewSchedule").Error(err)
-		return err
-	}
-
-	_, err = env.DB.Exec(query,
-		s.Name,
-		s.Description,
-		startUTC,
-		endUTC,
-		string(plans),
-		s.CreatedBy,
-	)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// RetrieveSchedule loads a schedule from database.
-func (env Env) RetrieveSchedule(id int64) (Schedule, error) {
-	query := fmt.Sprintf(`
-	%s
-	WHERE id = ?
-	LIMIT 1`, stmtDiscount)
-
-	var s Schedule
-	var plans string
-	var start string
-	var end string
-	var created string
-	err := env.DB.QueryRow(query, id).Scan(
-		&s.ID,
-		&s.Name,
-		&s.Description,
-		&start,
-		&end,
-		&plans,
-		&created,
-		&s.CreatedBy,
-	)
-
-	if err != nil {
-		logger.WithField("location", "RetrieveSchedule").Error(err)
-		return s, err
-	}
-
-	if err := json.Unmarshal([]byte(plans), &s.Plans); err != nil {
-		return s, err
-	}
-
-	s.Start = util.ISO8601UTC.FromDatetime(start, nil)
-	s.End = util.ISO8601UTC.FromDatetime(end, nil)
-	s.CreatedAt = util.ISO8601UTC.FromDatetime(created, nil)
-
-	return s, nil
-}
-
-// ListSchedules show all schedules.
-func (env Env) ListSchedules(page, rowCount int64) ([]Schedule, error) {
-	offset := (page - 1) * rowCount
-
-	query := fmt.Sprintf(`
-	%s
-	ORDER BY id DESC
-	LIMIT ? OFFSET ?`, stmtDiscount)
-
-	rows, err := env.DB.Query(query, rowCount, offset)
-
-	if err != nil {
-		logger.
-			WithField("location", "ListSchedules").
-			Error(err)
-
-		return nil, err
-	}
-	defer rows.Close()
-
-	schs := make([]Schedule, 0)
-
-	for rows.Next() {
-		var s Schedule
-		var plans string
-		var start string
-		var end string
-		var created string
-
-		err := rows.Scan(
-			&s.ID,
-			&s.Name,
-			&s.Description,
-			&start,
-			&end,
-			&plans,
-			&created,
-			&s.CreatedBy,
-		)
-
-		if err != nil {
-			logger.WithField("location", "ListDiscount").Error(err)
-
-			continue
-		}
-
-		if err := json.Unmarshal([]byte(plans), &s.Plans); err != nil {
-			continue
-		}
-
-		s.Start = util.ISO8601UTC.FromDatetime(start, nil)
-		s.End = util.ISO8601UTC.FromDatetime(end, nil)
-		s.CreatedAt = util.ISO8601UTC.FromDatetime(created, nil)
-
-		schs = append(schs, s)
-	}
-
-	if err := rows.Err(); err != nil {
-		logger.WithField("location", "ListDiscounts").Error(err)
-
-		return schs, err
-	}
-
-	return schs, nil
-}
-
-// DeleteSchedule delete a schedule row.
-func (env Env) DeleteSchedule(id int64) error {
-	query := `
-	DELETE FROM premium.discount_schedule
-	WHERE id = ?
+	UPDATE premium.promotion_schedule
+	SET plans = ?
+	WEHRE id = ?
 	LIMIT 1`
 
-	_, err := env.DB.Exec(query, id)
+	p, err := json.Marshal(plans)
 
 	if err != nil {
-		logger.WithField("location", "DeleteDiscount").Error(err)
+		logger.WithField("location", "NewPricing").Error(err)
+		return err
+	}
+
+	_, err = env.DB.Exec(query, string(p))
+
+	if err != nil {
+		logger.WithField("location", "NewPricing").Error(err)
 		return err
 	}
 
