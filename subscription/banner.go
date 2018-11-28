aa2d0789b77cd2cb4d63ed2a4a3a21b36d6d8f3d
weiguo.ni <neefrankie@gmail.com>
2018-11-28 16:15:03 +0800

    add CoverURL field

--- subscription/banner.go
+++ subscription/banner.go
@@ -9,6 +9,7 @@ import (
 
 // Banner is the content used on promotion banner
 type Banner struct {
+	CoverURL   string   `json:"coverUrl"`
 	Heading    string   `json:"heading"`    // Required. Max 256 chars.
 	SubHeading string   `json:"subHeading"` // Optional. Max 256 chars.
 	Content    []string `json:"content"`    // Optional.
@@ -16,12 +17,17 @@ type Banner struct {
 
 // Sanitize removes leading and trailing spaces.
 func (b *Banner) Sanitize() {
+	b.CoverURL = strings.TrimSpace(b.CoverURL)
 	b.Heading = strings.TrimSpace(b.Heading)
 	b.SubHeading = strings.TrimSpace(b.SubHeading)
 }
 
 // Validate validates input data for promotion banner.
 func (b *Banner) Validate() *util.Reason {
+	if r := util.OptionalMaxLen(b.CoverURL, 256, "coverUrl"); r != nil {
+		return r
+	}
+
 	if r := util.RequireNotEmptyWithMax(b.Heading, 256, "heading"); r != nil {
 		return r
 	}
