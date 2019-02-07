7ce76d00fcc8fc85b412ca5890f8dcc5ac865e77
weiguo.ni <neefrankie@gmail.com>
2019-02-07 22:04:08 +0800

    fix typo

--- util/errors.go
+++ util/errors.go
@@ -6,7 +6,7 @@ import (
 
 // Define common errors
 var (
-	ErrForbidden     = errors.New("acess denied") // Tells controller what kind of  HTTP response to use
+	ErrForbidden     = errors.New("access denied") // Tells controller what kind of  HTTP response to use
 	ErrWrongPassword = errors.New("wrong password")
 	ErrAlreadyExists = errors.New("already exists")
 )
