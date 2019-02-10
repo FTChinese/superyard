5984e03e82a1bb8566202262aca94166f8cf5b45
weiguo.ni <neefrankie@gmail.com>
2019-02-10 11:42:14 +0800

    mv sql to query file

--- model/oauth_client.go
+++ model/oauth_client.go
@@ -7,39 +7,6 @@ import (
 	"gitlab.com/ftchinese/backyard-api/util"
 )
 
-const (
-	stmtFTCApp = `
-	SELECT id AS id,
-		app_name AS appName,
-    	slug_name AS slugName,
-    	LOWER(HEX(client_id)) AS clientId,
-    	LOWER(HEX(client_secret)) AS clientSecret,
-    	repo_url AS repoUrl,
-    	description AS description,
-    	homepage_url AS homeUrl,
-		is_active AS isActive,
-		created_utc AS createdAt,
-		updated_utc AS updatedAt,
-    	owned_by AS ownedBy
-	FROM oauth.app_registry`
-
-	stmtPersonalToken = `
-	SELECT a.id AS id,
-		LOWER(HEX(a.access_token)) AS token,
-	    a.description AS description,
-	    u.email AS ftcEmail,
-	    a.created_by AS createdBy,
-		a.created_utc AS createdAt,
-		a.updated_utc AS updatedAt,
-		a.last_used_utc AS lastUsedAt
-	FROM oauth.access AS a
-		LEFT JOIN cmstmp01.userinfo AS u
-		ON a.myft_id = u.user_id
-	WHERE a.is_active = 1
-		AND a.created_by = ?
-		AND a.client_id IS NULL`
-)
-
 type OAuthEnv struct {
 	DB *sql.DB
 }
@@ -140,7 +107,7 @@ func (env OAuthEnv) ListApps(p util.Pagination) ([]oauth.App, error) {
 func (env OAuthEnv) LoadApp(slug string) (oauth.App, error) {
 	query := fmt.Sprintf(`
 	%s
-	WHERE app_slug = ?
+	WHERE slug_name = ?
 	LIMIT 1`, stmtFTCApp)
 
 	var app oauth.App
@@ -173,11 +140,11 @@ func (env OAuthEnv) UpdateApp(slug string, app oauth.App) error {
 	query := `
 	UPDATE oauth.app_registry
 	SET app_name = ?,
-	  	app_slug = ?,
+	  	slug_name = ?,
         repo_url = ?,
         description = ?,
         homepage_url = ?
-	WHERE app_slug = ?
+	WHERE slug_name = ?
 		AND owned_by = ?
       	AND is_active = 1
 	LIMIT 1`
