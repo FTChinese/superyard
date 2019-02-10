9523b8f6cc8b11f252f1a58c2946c80e59e18d6d
weiguo.ni <neefrankie@gmail.com>
2019-02-10 11:49:04 +0800

    more details for Order

--- user/order.go
+++ user/order.go
@@ -8,17 +8,20 @@ import (
 
 // Order is a user's subs order
 type Order struct {
-	ID            string         `json:"orderId"`
-	Tier          enum.Tier      `json:"tier"`
-	Cycle         enum.Cycle     `json:"cycle"`
-	ListPrice     float64        `json:"listPrice"`
-	NetPrice      float64        `json:"netPrice"`
-	PaymentMethod enum.PayMethod `json:"payMethod"`
-	CreatedAt     chrono.Time `json:"createdAt"`
-	ConfirmedAt   chrono.Time `json:"confirmedAt"`
-	StartDate     chrono.Date `json:"startDate"`
-	EndDate       chrono.Date `json:"endDate"`
-	ClientType    null.String      `json:"clientType"`
+	ID            string           `json:"orderId"`
+	UserID        string           `json:"userId"`
+	LoginMethod   enum.LoginMethod `json:"loginMethod"`
+	Tier          enum.Tier        `json:"tier"`
+	Cycle         enum.Cycle       `json:"cycle"`
+	ListPrice     float64          `json:"listPrice"`
+	NetPrice      float64          `json:"netPrice"`
+	PaymentMethod enum.PayMethod   `json:"payMethod"`
+	CreatedAt     chrono.Time      `json:"createdAt"`
+	ConfirmedAt   chrono.Time      `json:"confirmedAt"`
+	StartDate     chrono.Date      `json:"startDate"`
+	EndDate       chrono.Date      `json:"endDate"`
+	ClientType    enum.Platform      `json:"clientType"`
 	ClientVersion null.String      `json:"clientVersion"`
 	UserIP        null.String      `json:"userIp"`
+	UserAgent     null.String      `json:"userAgent"`
 }
