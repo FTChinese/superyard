5441631d199b2623ac98426585b8174766fc48a4
weiguo.ni <neefrankie@gmail.com>
2019-02-10 11:48:47 +0800

    Login

--- user/login.go
+++ user/login.go
@@ -1,1 +1,15 @@
 package user
+
+import "strings"
+
+// Login contains data to login to FTC
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// Sanitize removes leading and trailing spaces
+func (c *Login) Sanitize() {
+	c.Email = strings.TrimSpace(c.Email)
+	c.Password = strings.TrimSpace(c.Password)
+}
