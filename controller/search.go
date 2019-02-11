103aa2b27650436676cc2182a60f18b73d4d43fa
weiguo.ni <neefrankie@gmail.com>
2019-02-11 13:59:29 +0800

    search uesr and order

--- controller/search.go
+++ controller/search.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"database/sql"
 	"github.com/FTChinese/go-rest/view"
 	"gitlab.com/ftchinese/backyard-api/model"
 	"gitlab.com/ftchinese/backyard-api/user"
@@ -11,6 +12,11 @@ type SearchRouter struct {
 	model model.SearchEnv
 }
 
+func NewSearchRouter(db *sql.DB) SearchRouter {
+	return SearchRouter{
+		model: model.SearchEnv{DB: db},
+	}
+}
 // SearchUser tries to find a user by userName or email
 //
 //	GET /search/user?k=<name|email>&v=<value>
@@ -55,4 +61,31 @@ func (router SearchRouter) SearchUser(w http.ResponseWriter, req *http.Request) 
 	}
 
 	view.Render(w, view.NewResponse().NoCache().SetBody(u))
+}
+
+// SearchOrder tries to find an order by id.
+//
+//	GET /search/order?id=<string>
+func (router SearchRouter) SearchOrder(w http.ResponseWriter, req *http.Request)  {
+	err := req.ParseForm()
+
+	// 400 Bad Request
+	if err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	id, err := GetQueryParam(req, "id").ToString()
+	if err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	o, err := router.model.FindOrder(id)
+	if err != nil {
+		view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	view.Render(w, view.NewResponse().SetBody(o))
 }
