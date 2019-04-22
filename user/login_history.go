8646a91c1271beb3441dbe8f889675284a93fe2e
weiguo.ni <neefrankie@gmail.com>
2019-04-22 11:51:14 +0800

    refactor: use util.ClientApp

--- user/login_history.go
+++ user/login_history.go
@@ -3,12 +3,13 @@ package user
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
+	"gitlab.com/ftchinese/backyard-api/util"
 )
 
 // LoginHistory identifies how and from where the user login
 type LoginHistory struct {
 	UserID     string           `json:"userId"`
 	AuthMethod enum.LoginMethod `json:"loginMethod"`
-	ClientApp
+	util.ClientApp
 	CreatedAt chrono.Time `json:"createdAt"`
 }
