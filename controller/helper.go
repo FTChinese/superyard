18f9f10c161d03cd60f9d328a5fbce4a91096ac5
weiguo.ni <neefrankie@gmail.com>
2019-04-22 11:49:09 +0800

    refactor: replaced with gorest pkg

--- controller/helper.go
+++ controller/helper.go
@@ -1,75 +1,24 @@
 package controller
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"strconv"
-
+	gorest "github.com/FTChinese/go-rest"
 	"github.com/go-chi/chi"
 	"github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
 	"gitlab.com/ftchinese/backyard-api/util"
+	"io"
+	"io/ioutil"
+	"net/http"
 )
 
 var logger = log.WithField("project", "backyard-api").WithField("package", "controller")
 
-// Param represents a pair of query parameter from URL.
-type Param struct {
-	key   string
-	value string
-}
-
-// ToBool converts a query parameter to boolean value.
-func (p Param) ToBool() (bool, error) {
-	return strconv.ParseBool(p.value)
-}
-
-// ToString converts a query parameter to string value.
-// Returns error for an empty value.
-func (p Param) ToString() (string, error) {
-	if p.value == "" {
-		return "", fmt.Errorf("%s have empty value", p.key)
-	}
-
-	return p.value, nil
-}
-
-// ToInt converts the value of a query parameter to int64
-func (p Param) ToInt() (int64, error) {
-	if p.value == "" {
-		return 0, fmt.Errorf("%s have empty value", p.key)
-	}
-
-	num, err := strconv.ParseInt(p.value, 10, 0)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return num, nil
-}
-
-// GetQueryParam gets a pair of query parameter from URL.
-func GetQueryParam(req *http.Request, key string) Param {
-	v := req.Form.Get(key)
-
-	return Param{
-		key:   key,
-		value: v,
-	}
-}
-
 // GetURLParam gets a url parameter.
-func GetURLParam(req *http.Request, key string) Param {
+func GetURLParam(req *http.Request, key string) gorest.Param {
 	v := chi.URLParam(req, key)
 
-	return Param{
-		key:   key,
-		value: v,
-	}
+	return gorest.NewParam(key, v)
 }
 
 func GetJSONResult(data io.ReadCloser, path string) (gjson.Result, error) {
@@ -94,14 +43,4 @@ func IsAlreadyExists(err error) bool {
 	}
 
 	return false
-}
-
-func GetPagination(req *http.Request) util.Pagination {
-	page, _ := GetQueryParam(req, "page").ToInt()
-	perPage, err := GetQueryParam(req, "per_page").ToInt()
-	if err != nil {
-		perPage = 20
-	}
-
-	return util.NewPagination(page, perPage)
 }
