7593225f3d39894875ae2ee02ab487c0bf7773dc
weiguo.ni <neefrankie@gmail.com>
2019-05-05 16:39:48 +0800

    feat: model layer for apn content

---
+++ model/apn_article.go
@@ -0,0 +1,186 @@
+package model
+
+import (
+	"database/sql"
+	"github.com/guregu/null"
+	"gitlab.com/ftchinese/backyard-api/article"
+	"strings"
+)
+
+type APNEnv struct {
+	DB *sql.DB
+}
+
+func (env APNEnv) LatestStoryList() ([]article.Teaser, error) {
+	query := storyTeaser + `
+	WHERE story.pubdate = (SELECT pubdate
+			FROM cmstmp01.story
+			WHERE publish_status = 'publish'
+			ORDER BY pubdate DESC
+			LIMIT 1)
+		AND story.publish_status = 'publish'
+	ORDER BY story.last_publish_time DESC`
+
+	rows, err := env.DB.Query(query)
+
+	if err != nil {
+		logger.WithField("trace", "LatestStoryList").Error(err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	teasers := make([]article.Teaser, 0)
+
+	for rows.Next() {
+		var t article.Teaser
+		var tag string
+		err := rows.Scan(
+			&t.ArticleID,
+			&t.Title,
+			&t.Standfirst,
+			&t.Author,
+			&tag,
+			&t.CreatedAt,
+			&t.UpdateAt,
+			&t.CoverURL)
+
+		if err != nil {
+			logger.WithField("trace", "LatestStoryList").Error(err)
+			continue
+		}
+
+		t.Tags = strings.Split(tag, ",")
+
+		teasers = append(teasers, t)
+	}
+
+	if err := rows.Err(); err != nil {
+		logger.WithField("trace", "LatestStoryList").Error(err)
+		return teasers, err
+	}
+
+	return teasers, nil
+}
+
+func (env APNEnv) FindStory(id string) (article.Teaser, error) {
+	query := storyTeaser + `
+	WHERE story.id = ?;`
+
+	var t article.Teaser
+	var tag string
+	err := env.DB.QueryRow(query, id).Scan(
+		&t.ArticleID,
+		&t.Title,
+		&t.Standfirst,
+		&t.Author,
+		&tag,
+		&t.CreatedAt,
+		&t.UpdateAt,
+		&t.CoverURL)
+
+	if err != nil {
+		return t, err
+	}
+
+	t.Tags = strings.Split(tag, ",")
+	if !t.CoverURL.IsZero() {
+		t.CoverURL = null.StringFrom(strings.Replace(
+			t.CoverURL.String,
+			"/upload",
+			"http://i.ftimg.net",
+			1))
+	}
+	return t, nil
+}
+
+func (env APNEnv) FindVideo(id string) (article.Teaser, error) {
+	query := `
+	SELECT id AS id, 
+		cheadline AS title,
+		clongleadbody AS standfirst,
+		CONCAT(cdescribe, ' ', cbyline) AS author,
+		cc_piclink AS coverUrl
+	FROM cmstmp01.video_story
+	WHERE id = ? AND publish_status = 'publish'`
+
+	var t article.Teaser
+	err := env.DB.QueryRow(query, id).Scan(
+		&t.ArticleID,
+		&t.Title,
+		&t.Standfirst,
+		&t.Author,
+		&t.CoverURL)
+
+	if err != nil {
+		return t, err
+	}
+
+	return t, nil
+}
+
+func (env APNEnv) FindGallery(id string) (article.Teaser, error) {
+	query := `
+	SELECT photonewsid AS id, 
+		cn_title AS title,
+		leadbody AS standfirst,
+		tags,
+		FROM_UNIXTIME(add_times) AS createdAt,
+		thumb_url AS coverUrl
+	FROM cmstmp01.photonews
+	WHERE photonewsid = ?`
+
+	var t article.Teaser
+	var tag string
+	err := env.DB.QueryRow(query, id).Scan(
+		&t.ArticleID,
+		&t.Title,
+		&t.Standfirst,
+		&tag,
+		&t.CreatedAt,
+		&t.CoverURL)
+
+	if err != nil {
+		return t, err
+	}
+
+	t.Tags = strings.Split(tag, ",")
+	t.UpdateAt = t.CreatedAt
+
+	if !t.CoverURL.IsZero() {
+		t.CoverURL = null.StringFrom("http://i.ftimg.net/" + t.CoverURL.String)
+	}
+
+	return t, nil
+}
+
+func (env APNEnv) FindInteractive(id string) (article.Teaser, error) {
+	query := `
+	SELECT id AS id, 
+		cheadline AS title,
+		clongleadbody AS standfirst,
+		CONCAT(cbyline_description, ' ', cauthor) AS author,
+		tag,
+		FROM_UNIXTIME(fileupdatetime) AS updatedAt
+	FROM cmstmp01.interactive_story
+	WHERE id = ?`
+
+	var t article.Teaser
+	var tag string
+	err := env.DB.QueryRow(query, id).Scan(
+		&t.ArticleID,
+		&t.Title,
+		&t.Standfirst,
+		&t.Author,
+		&tag,
+		&t.UpdateAt)
+
+	if err != nil {
+		return t, err
+	}
+
+	t.Tags = strings.Split(tag, ",")
+	t.CreatedAt = t.UpdateAt
+
+	return t, nil
+}
---
+++ model/apn_article_test.go
@@ -0,0 +1,193 @@
+package model
+
+import (
+	"database/sql"
+	"testing"
+)
+
+func TestArticleEnv_LatestCover(t *testing.T) {
+	type fields struct {
+		DB *sql.DB
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name:    "Latest Cover Articles",
+			fields:  fields{DB: db},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := APNEnv{
+				DB: tt.fields.DB,
+			}
+			got, err := env.LatestStoryList()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("APNEnv.LatestStoryList() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("Teasers: %+v", got)
+		})
+	}
+}
+
+func TestArticleEnv_FindStory(t *testing.T) {
+	type fields struct {
+		DB *sql.DB
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Story Teaser",
+			fields:  fields{DB: db},
+			args:    args{id: "001076300"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := APNEnv{
+				DB: tt.fields.DB,
+			}
+			got, err := env.FindStory(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("APNEnv.FindStory() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("Story teaser: %+v", got)
+		})
+	}
+}
+
+func TestArticleEnv_FindVideo(t *testing.T) {
+	type fields struct {
+		DB *sql.DB
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		//want    article.Teaser
+		wantErr bool
+	}{
+		{
+			name:    "Video Teaser",
+			fields:  fields{DB: db},
+			args:    args{id: "2586"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := APNEnv{
+				DB: tt.fields.DB,
+			}
+			got, err := env.FindVideo(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("APNEnv.FindVideo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("APNEnv.FindVideo() = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("Video teaser: %+v", got)
+		})
+	}
+}
+
+func TestArticleEnv_FindGallery(t *testing.T) {
+	type fields struct {
+		DB *sql.DB
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		//want    article.Teaser
+		wantErr bool
+	}{
+		{
+			name:    "Gallery Teaser",
+			fields:  fields{DB: db},
+			args:    args{id: "1050"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := APNEnv{
+				DB: tt.fields.DB,
+			}
+			got, err := env.FindGallery(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("APNEnv.FindGallery() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("APNEnv.FindGallery() = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("Gallery teaser: %+v", got)
+		})
+	}
+}
+
+func TestArticleEnv_FindInteractive(t *testing.T) {
+	type fields struct {
+		DB *sql.DB
+	}
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		//want    article.Teaser
+		wantErr bool
+	}{
+		{
+			name:    "Interactive Teaser",
+			fields:  fields{DB: db},
+			args:    args{id: "5065"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := APNEnv{
+				DB: tt.fields.DB,
+			}
+			got, err := env.FindInteractive(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("APNEnv.FindInteractive() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("APNEnv.FindInteractive() = %v, want %v", got, tt.want)
+			//}
+
+			t.Logf("Interactive teaser: %+v", got)
+		})
+	}
+}
