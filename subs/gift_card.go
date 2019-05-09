0d7bcbe66ca6960aca9aa985428da2d8fe0da038
weiguo.ni <neefrankie@gmail.com>
2019-05-09 15:01:44 +0800

    feat: search gift card

---
+++ subs/gift_card.go
@@ -0,0 +1,16 @@
+package subs
+
+import (
+	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/enum"
+)
+
+type GiftCard struct {
+	ID         int64       `json:"id"`
+	Serial     string      `json:"serial"`
+	ExpireDate chrono.Date `json:"expireDate"`
+	RedeemedAt chrono.Time `json:"redeemedAt"`
+	Tier       enum.Tier   `json:"tier"`
+	CycleUnit  enum.Cycle  `json:"cycleUnit"`
+	CycleCount int64       `json:"cycleCount"`
+}
