6cabb0a194b909c37dd1408b360ed50d1cd37460
weiguo.ni <neefrankie@gmail.com>
2019-06-26 18:38:02 +0800

    feat: sql statement for android release

---
+++ query/android_release.go
@@ -0,0 +1,42 @@
+package query
+
+const (
+	InsertRelease = `
+	INSERT INTO file_store.android_release
+		SET version_name = ?,
+			version_code = ?,
+			body = ?,
+			binary_url = ?,
+			created_utc = UTC_TIMESTAMP(),
+			updated_utc = UTC_TIMESTAMP()`
+
+	androidRelease = `SELECT version_name,
+		version_code,
+		body,
+		binary_url,
+		created_utc,
+		updated_utc
+	FROM file_store.android_release`
+
+	SingleRelease = androidRelease + `
+	WHERE version_name = ?
+	LIMIT 1`
+
+	AllReleases = androidRelease + `
+	ORDER BY version_code DESC
+	LIMIT ? OFFSET ?`
+
+	UpdateRelease = `
+	UPDATE file_store.android_release
+	SET version_code = ?,
+		body = ?,
+		binary_url = ?,
+		updated_utc = UTC_TIMESTAMP()
+	WHERE version_name = ?
+	LIMIT 1`
+
+	DeleteRelease = `
+	DELETE FROM file_store.android_release
+	WHERE version_name = ?
+	LIMIT 1`
+)
