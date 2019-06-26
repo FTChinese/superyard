68021206f60f4516e27a0e6e621cd1f16c2032d0
weiguo.ni <neefrankie@gmail.com>
2019-06-26 18:38:34 +0800

    refactor: utf8mb4

--- util/db.go
+++ util/db.go
@@ -16,20 +16,32 @@ type Conn struct {
 	Pass string `mapstructure:"pass"`
 }
 
-// NewDB creates a db connection
-func NewDB(c Conn) (*sql.DB, error) {
+func (c Conn) DSN() string {
 	cfg := &mysql.Config{
 		User:   c.User,
 		Passwd: c.Pass,
 		Net:    "tcp",
 		Addr:   fmt.Sprintf("%s:%d", c.Host, c.Port),
+		// Always use UTC time.
+		// Pay attention to how string values are specified.
+		// The string value provided to MySQL must be quoted in single quote for this driver to work,
+		// which means the single quote itself must be included in the string value.
+		// The resulting string value passed to MySQL should look like: `%27<you string value>%27`
+		// See ASCII Encoding Reference https://www.w3schools.com/tags/ref_urlencode.asp
 		Params: map[string]string{
 			"time_zone": `'+00:00'`,
 		},
+		Collation:            "utf8mb4_unicode_ci",
 		AllowNativePasswords: true,
 	}
 
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	return cfg.FormatDSN()
+}
+
+// NewDB creates a db connection
+func NewDB(c Conn) (*sql.DB, error) {
+
+	db, err := sql.Open("mysql", c.DSN())
 
 	if err != nil {
 		return nil, err
