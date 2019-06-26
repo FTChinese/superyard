fcb294f5dc61bbaf92255121a901c483565d0e52
weiguo.ni <neefrankie@gmail.com>
2019-06-26 18:37:21 +0800

    feat: android release data

---
+++ android/release.go
@@ -0,0 +1,44 @@
+package android
+
+import (
+	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/view"
+	"github.com/guregu/null"
+	"gitlab.com/ftchinese/backyard-api/util"
+	"strings"
+)
+
+type Release struct {
+	VersionName string      `json:"versionName"`
+	VersionCode int64       `json:"versionCode"`
+	Body        null.String `json:"body"`
+	BinaryURL   string      `json:"binaryUrl"`
+	CreatedAt   chrono.Time `json:"createdAt"`
+	UpdatedAt   chrono.Time `json:"updatedAt"`
+}
+
+func (r *Release) Sanitize() {
+	r.VersionName = strings.TrimSpace(r.VersionName)
+	r.Body.String = strings.TrimSpace(r.Body.String)
+	r.BinaryURL = strings.TrimSpace(r.BinaryURL)
+}
+
+func (r Release) Validate() *view.Reason {
+	if r.VersionCode < 1 {
+		r := view.NewReason()
+		r.Field = "versionCode"
+		r.Code = view.CodeInvalid
+		r.SetMessage("version code must be larger than 0")
+		return r
+	}
+
+	if r := util.RequireNotEmptyWithMax(r.VersionName, 32, "versionName"); r != nil {
+		return r
+	}
+
+	if r := util.RequireNotEmpty(r.BinaryURL, "binaryUrl"); r != nil {
+		return r
+	}
+
+	return nil
+}
---
+++ controller/android.go
@@ -0,0 +1,167 @@
+package controller
+
+import (
+	"database/sql"
+	gorest "github.com/FTChinese/go-rest"
+	"github.com/FTChinese/go-rest/view"
+	"gitlab.com/ftchinese/backyard-api/android"
+	"gitlab.com/ftchinese/backyard-api/model"
+	"net/http"
+)
+
+type AndroidRouter struct {
+	model model.AndroidEnv
+}
+
+func NewAndroidRouter(db *sql.DB) AndroidRouter {
+	return AndroidRouter{
+		model: model.AndroidEnv{
+			DB: db,
+		},
+	}
+}
+
+// CreateRelease inserts the metadata for a new Android release.
+//
+// POST /android/releases
+//
+// Body: {versionName: string, versionCode: int, body: string, binaryUrl: string}
+func (router AndroidRouter) CreateRelease(w http.ResponseWriter, req *http.Request) {
+	var r android.Release
+
+	if err := gorest.ParseJSON(req.Body, &r); err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	r.Sanitize()
+
+	if reason := r.Validate(); reason != nil {
+		view.Render(w, view.NewUnprocessable(reason))
+		return
+	}
+
+	err := router.model.CreateRelease(r)
+	if err != nil {
+		if IsAlreadyExists(err) {
+			reason := view.NewReason()
+			reason.Field = "versionName"
+			reason.Code = view.CodeAlreadyExists
+			view.Render(w, view.NewUnprocessable(reason))
+			return
+		}
+
+		view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	view.Render(w, view.NewNoContent())
+}
+
+// Releases retrieves all releases by sorting version code
+// in descending order.
+//
+// GET /android/releases?page=<number>&per_page=<number>
+func (router AndroidRouter) Releases(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+
+	if err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	pagination := gorest.GetPagination(req)
+
+	releases, err := router.model.ListReleases(pagination)
+
+	if err != nil {
+		view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	view.Render(w, view.NewResponse().SetBody(releases))
+}
+
+// SingleReleases retrieves a release by version name
+//
+// GET /android/releases/{versionName}
+func (router AndroidRouter) SingleRelease(w http.ResponseWriter, req *http.Request) {
+	versionName, err := GetURLParam(req, "versionName").ToString()
+
+	if err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	release, err := router.model.SingleRelease(versionName)
+	if err != nil {
+		view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	view.Render(w, view.NewResponse().SetBody(release))
+}
+
+// UpdateRelease updates a single release.
+//
+// PATCH /android/releases/{versionName}
+//
+// Body {versionName: string, versionCode: int, body: string, binaryUrl: string}
+func (router AndroidRouter) UpdateRelease(w http.ResponseWriter, req *http.Request) {
+	versionName, err := GetURLParam(req, "vesionName").ToString()
+
+	if err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	var release android.Release
+	if err := gorest.ParseJSON(req.Body, &release); err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	release.Sanitize()
+
+	if r := release.Validate(); r != nil {
+		view.Render(w, view.NewUnprocessable(r))
+		return
+	}
+
+	err = router.model.UpdateRelease(release, versionName)
+
+	if err != nil {
+		if IsAlreadyExists(err) {
+			r := view.NewReason()
+			r.Field = "versionCode"
+			r.Code = view.CodeAlreadyExists
+			r.SetMessage("versionCode already exists")
+			view.Render(w, view.NewUnprocessable(r))
+			return
+		}
+
+		view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	view.Render(w, view.NewNoContent())
+}
+
+// DeleteRelease deletes a single release
+//
+// DELETE /android/releases/:versionName
+func (router AndroidRouter) DeleteRelease(w http.ResponseWriter, req *http.Request) {
+	versionName, err := GetURLParam(req, "versionName").ToString()
+	if err != nil {
+		view.Render(w, view.NewBadRequest(err.Error()))
+		return
+	}
+
+	err = router.model.DeleteRelease(versionName)
+	if err != nil {
+		view.Render(w, view.NewDBFailure(err))
+		return
+	}
+
+	view.Render(w, view.NewNoContent())
+}
