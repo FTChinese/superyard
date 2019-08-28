d1f3cf8c5ef65cf0e9ba259242870e1c122d7238
weiguo.ni <neefrankie@gmail.com>
2019-08-28 14:22:39 +0800

    refactor: removed normalizeMemberTier

--- repository/customer/env.go
+++ repository/customer/env.go
@@ -1,7 +1,6 @@
 package customer
 
 import (
-	"github.com/FTChinese/go-rest/enum"
 	"github.com/jmoiron/sqlx"
 	"github.com/sirupsen/logrus"
 )
@@ -12,17 +11,3 @@ type Env struct {
 }
 
 var logger = logrus.WithField("package", "repository.customer")
-
-func normalizeMemberTier(vipType int64) enum.Tier {
-	switch vipType {
-
-	case 10:
-		return enum.TierStandard
-
-	case 100:
-		return enum.TierPremium
-
-	default:
-		return enum.InvalidTier
-	}
-}
