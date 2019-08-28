5154698cdde27396504999ff083767faffbf0afa
weiguo.ni <neefrankie@gmail.com>
2019-08-28 17:52:11 +0800

    refactor: Account incorporates both ftc and wx

--- models/reader/account.go
+++ models/reader/account.go
@@ -1,8 +1,17 @@
 package reader
 
-// Account shows a user's core data.
+import (
+	"github.com/guregu/null"
+)
+
+// Account contains a complete user account, consisting of
+// both ftc account and wechat account.
 type Account struct {
-	Ftc        FtcAccount `json:"ftc"`
-	Wechat     WxAccount  `json:"wechat"`
-	Membership Membership `json:"membership"`
+	FtcID      null.String `json:"ftcId" db:"ftc_id"`
+	UnionID    null.String `json:"unionId" db:"union_id"`
+	StripeID   null.String `json:"stripeId" db:"stripe_id"`
+	Email      null.String `json:"email" db:"email"`
+	UserName   null.String `json:"userName" db:"user_name"`
+	Nickname   null.String `json:"nickname" db:"nickname"`
+	Membership Membership  `json:"membership"`
 }
