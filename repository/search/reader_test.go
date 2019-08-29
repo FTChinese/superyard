55ed0066928b01c63640959c64fa242c847fa680
weiguo.ni <neefrankie@gmail.com>
2019-08-29 13:32:20 +0800

    test

---
+++ repository/search/reader_test.go
@@ -0,0 +1,73 @@
+package search
+
+import (
+	"github.com/FTChinese/go-rest"
+	"gitlab.com/ftchinese/backyard-api/test"
+	"testing"
+)
+
+func TestEnv_SearchFtcUser(t *testing.T) {
+	env := Env{DB: test.DBX}
+
+	type args struct {
+		email string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "Search FTC User",
+			args:    args{email: test.MyProfile.Email},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := env.SearchFtcUser(tt.args.email)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SearchFtcUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("Ftc user: %+v", got)
+		})
+	}
+}
+
+func TestEnv_SearchWxUser(t *testing.T) {
+
+	env := Env{DB: test.DBX}
+
+	type args struct {
+		nickname string
+		p        gorest.Pagination
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Search wechat user",
+			args: args{
+				nickname: test.MyProfile.Nickname,
+				p:        gorest.NewPagination(1, 10),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := env.SearchWxUser(tt.args.nickname, tt.args.p)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SearchWxUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("Wechat user: %+v", got)
+		})
+	}
+}
