eaee248ee2b1e15c4da791c71b9119bc97d205b0
weiguo.ni <neefrankie@gmail.com>
2019-08-30 15:44:18 +0800

    renamed

--- repository/staff/profile.go
+++ repository/staff/profile.go
@@ -3,27 +3,15 @@ package staff
 import (
 	"github.com/FTChinese/go-rest"
 	"gitlab.com/ftchinese/backyard-api/models/employee"
-	"gitlab.com/ftchinese/backyard-api/models/util"
 )
 
-func (env Env) Create(a employee.Account) error {
-	_, err := env.DB.NamedExec(stmtInsertEmployee, &a)
-
-	if err != nil {
-		logger.WithField("trace", "Env.CreateAccount").Error(err)
-		return err
-	}
-
-	return nil
-}
-
-func (env Env) Load(col Column, value string) (employee.Profile, error) {
+func (env Env) RetrieveProfile(id string) (employee.Profile, error) {
 	var p employee.Profile
 
-	err := env.DB.Get(&p, queryProfile(col), value)
+	err := env.DB.Get(&p, stmtSelectProfile, id)
 
 	if err != nil {
-		logger.WithField("trace", "Env.LoadProfile").Error(err)
+		logger.WithField("trace", "Env.RetrieveProfile").Error(err)
 
 		return p, err
 	}
@@ -31,7 +19,7 @@ func (env Env) Load(col Column, value string) (employee.Profile, error) {
 	return p, nil
 }
 
-func (env Env) List(p gorest.Pagination) ([]employee.Profile, error) {
+func (env Env) ListStaff(p gorest.Pagination) ([]employee.Profile, error) {
 	profiles := make([]employee.Profile, 0)
 
 	err := env.DB.Select(&profiles,
@@ -40,7 +28,7 @@ func (env Env) List(p gorest.Pagination) ([]employee.Profile, error) {
 		p.Offset())
 
 	if err != nil {
-		logger.WithField("trace", "Env.List").Error(err)
+		logger.WithField("trace", "Env.ListStaff").Error(err)
 
 		return profiles, err
 	}
@@ -53,7 +41,7 @@ func (env Env) List(p gorest.Pagination) ([]employee.Profile, error) {
 //	PATCH /admin/accounts/{name}
 //
 // Input {userName: string, email: string, displayName: string, department: string, groupMembers: number}
-func (env Env) Update(p employee.Profile) error {
+func (env Env) UpdateProfile(p employee.Profile) error {
 	_, err := env.DB.NamedExec(stmtUpdateProfile, &p)
 	if err != nil {
 		logger.WithField("trace", "Env.UpdateAccount").Error(err)
@@ -63,34 +51,18 @@ func (env Env) Update(p employee.Profile) error {
 	return nil
 }
 
-// UpdateDisplayName allows a user to change its display name.
-// PATCH /user/display-name
-func (env Env) UpdateDisplayName(displayName, staffID string) error {
-	_, err := env.DB.Exec(
-		stmtUpdateName,
-		displayName,
-		staffID)
+// VerifyPassword verifies a staff's password and returns
+// account data if it is correct.
+func (env Env) VerifyPassword(a employee.Account) (employee.Account, error) {
+	var account employee.Account
+	err := env.DB.Get(&account, stmtVerifyPassword, a.ID, a.Password)
 
 	if err != nil {
-		logger.WithField("trace", "Env.UpdateDisplayName").Error(err)
-
-		return err
+		logger.WithField("trace", "VerifyPassword").Error(err)
+		return employee.Account{}, err
 	}
 
-	return nil
-}
-
-// UpdateEmail allows a user to update its email address.
-func (env Env) UpdateEmail(email, staffID string) error {
-	_, err := env.DB.Exec(stmtUpdateEmail, email, staffID)
-
-	if err != nil {
-		logger.WithField("trace", "Env.UpdateEmail").Error(err)
-
-		return err
-	}
-
-	return nil
+	return account, nil
 }
 
 // Change password is used by both UpdatePassword after user logged in, or reset password if user forgot it.
@@ -124,21 +96,31 @@ func (env Env) changePassword(password string, userName string) error {
 }
 
 // UpdatePassword allows user to change password in its settings.
-func (env Env) UpdatePassword(p employee.Password, userName string) error {
-	matched, err := env.VerifyPassword(employee.Login{
-		UserName: userName,
-		Password: p.Old,
-	})
+func (env Env) UpdatePassword(a employee.Account) error {
 
+	tx, err := env.DB.Beginx()
 	if err != nil {
 		return err
 	}
 
-	if !matched {
-		return util.ErrWrongPassword
+	// Update password in the new table.
+	_, err = tx.NamedExec(stmtUpdatePassword, a)
+	if err != nil {
+		_ = tx.Rollback()
+		logger.WithField("trace", "Env.UpdatePassword").Error(err)
+		return err
 	}
 
-	if err := env.changePassword(p.New, userName); err != nil {
+	// Update password in old table
+	_, err = tx.NamedExec(stmtUpdateLegacyPassword, a)
+	if err != nil {
+		_ = tx.Rollback()
+		logger.WithField("trace", "Env.UpdatePassword").Error(err)
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		logger.WithField("trace", "Env.UpdatePassword").Error(err)
 		return err
 	}
 
