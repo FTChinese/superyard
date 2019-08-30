9380161ab10d03dded20d5f752a9ff64d3f27096
weiguo.ni <neefrankie@gmail.com>
2019-08-30 16:17:24 +0800

    rename

---
+++ models/builder/search_param.go
@@ -0,0 +1,44 @@
+package builder
+
+import (
+	"errors"
+	"strings"
+)
+
+// SearchParam is an experimental attempt to receive form
+// value using gorilla schema package.
+type SearchParam struct {
+	Email string `schema:"email"`
+	Name  string `schema:"name"`
+	Q     string `schema:"q"`
+}
+
+func (p *SearchParam) Sanitize() {
+	p.Email = strings.TrimSpace(p.Email)
+	p.Name = strings.TrimSpace(p.Name)
+	p.Q = strings.TrimSpace(p.Q)
+}
+
+func (p SearchParam) RequireEmail() error {
+	if p.Email == "" {
+		return errors.New("email is required")
+	}
+
+	return nil
+}
+
+func (p SearchParam) NameOrEmail() error {
+	if p.Email == "" && p.Name == "" {
+		return errors.New("email or name should be specified")
+	}
+
+	return nil
+}
+
+func (p SearchParam) RequireQ() error {
+	if p.Q == "" {
+		return errors.New("missing query parameter 'q'")
+	}
+
+	return nil
+}
