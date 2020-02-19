e6ef9da837128fbf6ad786b46ede4b61f03552f9
neefrankie <neefrankie@gmail.com>
2020-02-19 10:36:36 +0800

    refactor: create Client type

--- models/reader/login_history.go
+++ models/reader/login_history.go
@@ -4,7 +4,6 @@ import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
 	"github.com/guregu/null"
-	"gitlab.com/ftchinese/superyard/models/util"
 )
 
 type ActivityKind string
@@ -17,15 +16,19 @@ const (
 	ActivityKindPasswordReset              = "password_reset"
 )
 
+type Client struct {
+	Platform  enum.Platform `json:"platform" db:"platform"`
+	Version   null.String   `json:"version" db:"version"`
+	UserIP    null.String   `json:"userIp" db:"user_ip"`
+	UserAgent null.String   `json:"userAgent" db:"user_agent"`
+}
+
 // Activity shows a user's footprint when using email account.
 type Activity struct {
-	FtcID      string        `json:"ftcId" db:"ftc_id"`
-	Platform   enum.Platform `json:"platform" db:"platform"`
-	Version    null.String   `json:"version" db:"version"`
-	UserIP     null.String   `json:"userIp" db:"user_ip"`
-	UserAgent  null.String   `json:"userAgent" db:"user_agent"`
-	CreatedUTC chrono.Time   `json:"createdUtc" db:"created_utc"`
-	Kind       ActivityKind  `json:"kind" db:"kind"`
+	FtcID string `json:"ftcId" db:"ftc_id"`
+	Client
+	CreatedUTC chrono.Time  `json:"createdUtc" db:"created_utc"`
+	Kind       ActivityKind `json:"kind" db:"kind"`
 }
 
 // OAuthHistory is a record every time user logged in
@@ -34,7 +37,7 @@ type OAuthHistory struct {
 	UnionID string `json:"unionId" db:"union_id"`
 	OpenID  string `json:"openId" db:"open_id"`
 	AppID   string `json:"appId" db:"app_id"`
-	util.ClientApp
+	Client
 	CreatedAt chrono.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt chrono.Time `json:"updatedAt" db:"updated_at"`
 }
