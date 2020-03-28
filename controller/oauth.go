250d560be68a0ddcb910ce9972830d925333dff8
neefrankie <neefrankie@gmail.com>
2020-03-28 11:26:51 +0800

    refactor: rename

--- controller/oauth.go
+++ controller/oauth.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"gitlab.com/ftchinese/superyard/repository/admin"
 	"net/http"
 
 	"github.com/FTChinese/go-rest/render"
@@ -8,32 +9,27 @@ import (
 	"github.com/labstack/echo/v4"
 	"gitlab.com/ftchinese/superyard/models/oauth"
 	"gitlab.com/ftchinese/superyard/models/util"
-	"gitlab.com/ftchinese/superyard/models/validator"
 	"gitlab.com/ftchinese/superyard/repository/registry"
-	"gitlab.com/ftchinese/superyard/repository/staff"
 )
 
-type ApiRouter struct {
-	model registry.Env
-	staff staff.Env
+type OAuthRouter struct {
+	regRepo   registry.Env
+	adminRepo admin.Env
 }
 
-// APIRouter creates a new instance of FTCAPIRouter.
-func APIRouter(db *sqlx.DB) ApiRouter {
-	return ApiRouter{
-		model: registry.Env{DB: db},
-		staff: staff.Env{DB: db},
+// NewOAuthRouter creates a new instance of FTCAPIRouter.
+func NewOAuthRouter(db *sqlx.DB) OAuthRouter {
+	return OAuthRouter{
+		regRepo:   registry.Env{DB: db},
+		adminRepo: admin.Env{DB: db},
 	}
 }
 
 // CreateApp creates an new app which needs to access next-api.
 //
-//	POST /next/apps
-//
 // Input {name: string, slug: string, repoUrl: string, description: string, homeUrl: string}
-func (router ApiRouter) CreateApp(c echo.Context) error {
-	// TODO: use JWT.
-	userName := c.Request().Header.Get(userNameKey)
+func (router OAuthRouter) CreateApp(c echo.Context) error {
+	claims := getAccountClaims(c)
 
 	var input oauth.BaseApp
 	if err := c.Bind(&input); err != nil {
@@ -52,9 +48,9 @@ func (router ApiRouter) CreateApp(c echo.Context) error {
 	if err != nil {
 		return render.NewBadRequest(err.Error())
 	}
-	app.OwnedBy = userName
+	app.OwnedBy = claims.Username
 
-	err = router.model.CreateApp(app)
+	err = router.regRepo.CreateApp(app)
 	if err != nil {
 		if util.IsAlreadyExists(err) {
 			return render.NewAlreadyExists("slug")
@@ -68,7 +64,7 @@ func (router ApiRouter) CreateApp(c echo.Context) error {
 // ListApps loads all app with pagination support
 //
 //	GET /next/apps?page=<number>&per_page=<number>
-func (router ApiRouter) ListApps(c echo.Context) error {
+func (router OAuthRouter) ListApps(c echo.Context) error {
 
 	var pagination util.Pagination
 	if err := c.Bind(&pagination); err != nil {
@@ -76,7 +72,7 @@ func (router ApiRouter) ListApps(c echo.Context) error {
 	}
 	pagination.Normalize()
 
-	apps, err := router.model.ListApps(pagination)
+	apps, err := router.regRepo.ListApps(pagination)
 	if err != nil {
 		return render.NewDBError(err)
 	}
@@ -84,11 +80,11 @@ func (router ApiRouter) ListApps(c echo.Context) error {
 	return c.JSON(http.StatusOK, apps)
 }
 
-// RetrieveApp retrieves an app by its slug name.
-func (router ApiRouter) LoadApp(c echo.Context) error {
+// RetrieveApp retrieves an app by its client id.
+func (router OAuthRouter) LoadApp(c echo.Context) error {
 	clientID := c.Param("id")
 
-	app, err := router.model.RetrieveApp(clientID)
+	app, err := router.regRepo.RetrieveApp(clientID)
 	if err != nil {
 		return render.NewDBError(err)
 	}
@@ -97,27 +93,31 @@ func (router ApiRouter) LoadApp(c echo.Context) error {
 }
 
 // UpdateApp updates an app's data.
+// Any logged in user could update any app, regardless who owned it.
 //
 //	PATCH /api/apps/:id
 //
-// Input {name: string, slug: string, repoUrl?: string, description?: string, homeUrl?: string, callbackUrl?: string, ownedBy: string}
-func (router ApiRouter) UpdateApp(c echo.Context) error {
+// Input {name: string, slug: string, repoUrl?: string, description?: string, homeUrl?: string, callbackUrl?: string}
+func (router OAuthRouter) UpdateApp(c echo.Context) error {
 
 	clientID := c.Param("id")
 
-	var app oauth.App
-	if err := c.Bind(&app); err != nil {
+	var input oauth.BaseApp
+	if err := c.Bind(&input); err != nil {
 		return render.NewBadRequest(err.Error())
 	}
 
-	app.Sanitize()
-	if ve := app.Validate(); ve != nil {
+	input.Sanitize()
+	if ve := input.Validate(); ve != nil {
 		return render.NewUnprocessable(ve)
 	}
 
-	app.ClientID = clientID
+	app := oauth.App{
+		BaseApp:  input,
+		ClientID: clientID,
+	}
 
-	if err := router.model.UpdateApp(app); err != nil {
+	if err := router.regRepo.UpdateApp(app); err != nil {
 		if util.IsAlreadyExists(err) {
 			return render.NewAlreadyExists("slug")
 		}
@@ -129,21 +129,14 @@ func (router ApiRouter) UpdateApp(c echo.Context) error {
 
 // RemoveApp flags an app as inactive.
 // This also removes all access tokens owned by this app.
+// We does not impose access control here.
+// Anyone can delete any app created by others.
 //
 //	DELETE /api/apps/:id
-// Input: {ownedBy: string}
-func (router ApiRouter) RemoveApp(c echo.Context) error {
-
+func (router OAuthRouter) RemoveApp(c echo.Context) error {
 	clientID := c.Param("id")
 
-	var by oauth.AppRemover
-	if err := c.Bind(&by); err != nil {
-		return render.NewBadRequest(err.Error())
-	}
-
-	by.ClientID = clientID
-
-	if err := router.model.RemoveApp(by); err != nil {
+	if err := router.regRepo.RemoveApp(clientID); err != nil {
 		return render.NewDBError(err)
 	}
 
@@ -151,12 +144,16 @@ func (router ApiRouter) RemoveApp(c echo.Context) error {
 }
 
 // ListKeys shows all access tokens owned by an app or by a human.
-func (router ApiRouter) ListKeys(c echo.Context) error {
+// Query params:
+// ?client_id=<string>&page=<number>&per_page=<number>
+// All are optional.
+// Is client_id is present, it indicates the client
+// is requesting tokens belong to an app; otherwise it
+// indicates personal keys.
+func (router OAuthRouter) ListKeys(c echo.Context) error {
 
-	var selector oauth.KeySelector
-	if err := c.Bind(&selector); err != nil {
-		return render.NewBadRequest(err.Error())
-	}
+	clientID := c.QueryParam("client_id")
+	claims := getAccountClaims(c)
 
 	var p util.Pagination
 	if err := c.Bind(&p); err != nil {
@@ -164,7 +161,14 @@ func (router ApiRouter) ListKeys(c echo.Context) error {
 	}
 	p.Normalize()
 
-	tokens, err := router.model.ListKeys(selector, p)
+	var tokens []oauth.Access
+	var err error
+	if clientID != "" {
+		tokens, err = router.regRepo.ListAccessTokens(clientID, p)
+	} else {
+		tokens, err = router.regRepo.ListPersonalKeys(claims.Username, p)
+	}
+
 	if err != nil {
 		return render.NewDBError(err)
 	}
@@ -173,8 +177,9 @@ func (router ApiRouter) ListKeys(c echo.Context) error {
 }
 
 // NewToken creates an access token for a person or for an app.
-// Input: {description?: string, createdBy: string, clientId?: string}
-func (router ApiRouter) CreateKey(c echo.Context) error {
+// Input: {description?: string, clientId?: string}
+func (router OAuthRouter) CreateKey(c echo.Context) error {
+	claims := getAccountClaims(c)
 
 	var input oauth.BaseAccess
 	if err := c.Bind(&input); err != nil {
@@ -186,33 +191,13 @@ func (router ApiRouter) CreateKey(c echo.Context) error {
 		return render.NewUnprocessable(ve)
 	}
 
-	acc, err := oauth.NewAccess(input)
+	acc, err := oauth.NewAccess(input, claims.Username)
 	if err != nil {
 		return render.NewBadRequest(err.Error())
 	}
 
-	_, err = router.model.CreateToken(acc)
+	_, err = router.regRepo.CreateToken(acc)
 	if err != nil {
-		return render.NewDBError(err)
-	}
-
-	return c.NoContent(http.StatusNoContent)
-}
-
-// DeletePersonalKeys deactivates all keys owned by a user.
-// Input: { createdBy: string}
-func (router ApiRouter) DeletePersonalKeys(c echo.Context) error {
-	var by oauth.KeyRemover
-	if err := c.Bind(&by); err != nil {
-		return render.NewBadRequest(err.Error())
-	}
-
-	ve := validator.New("createdBy").Required().Validate(by.CreatedBy)
-	if ve != nil {
-		return render.NewUnprocessable(ve)
-	}
-
-	if err := router.model.DeleteKeys(by.CreatedBy); err != nil {
 		return render.NewDBError(err)
 	}
 
@@ -221,22 +206,20 @@ func (router ApiRouter) DeletePersonalKeys(c echo.Context) error {
 
 // RemoveKey deactivate an access token created by a user.
 // The token could be owned by either an app or a human being.
-// Input { createdBy: string}.
-// The input restricts that user could only delete keys created by itself.
-func (router ApiRouter) RemoveKey(c echo.Context) error {
+func (router OAuthRouter) RemoveKey(c echo.Context) error {
+	claims := getAccountClaims(c)
+
 	id, err := ParseInt(c.Param("id"))
 	if err != nil {
 		return render.NewBadRequest(err.Error())
 	}
 
-	var by oauth.KeyRemover
-	if err := c.Bind(&by); err != nil {
-		return render.NewBadRequest(err.Error())
+	key := oauth.Access{
+		ID:        id,
+		CreatedBy: claims.Username,
 	}
 
-	by.ID = id
-
-	if err := router.model.RemoveKey(by); err != nil {
+	if err := router.regRepo.RemoveKey(key); err != nil {
 		return render.NewDBError(err)
 	}
 
