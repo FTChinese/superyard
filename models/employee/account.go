4b261f2b4a5768a1b54d217e3ac4338d3a8b6098
neefrankie <neefrankie@gmail.com>
2020-03-28 11:16:41 +0800

    refactor: create BaseAccount for shared fields; create Credentials to hold user identity and password;

--- models/employee/account.go
+++ models/employee/account.go
@@ -1,9 +1,7 @@
 package employee
 
 import (
-	"github.com/FTChinese/go-rest"
 	"github.com/FTChinese/go-rest/postoffice"
-	"github.com/FTChinese/go-rest/rand"
 	"github.com/FTChinese/go-rest/render"
 	"github.com/guregu/null"
 	"gitlab.com/ftchinese/superyard/models/validator"
@@ -11,40 +9,48 @@ import (
 	"text/template"
 )
 
-func GenerateID() string {
-	return "stf_" + rand.String(12)
-}
-
-// Account contains essential data of a user.
-// It is used as response data for user authentication.
-// It is also used to create a new user. In this case, password is set to a random string and sent to the Email of this new user. You must make sure the email already works.
-type Account struct {
-	ID           null.String `json:"id" db:"staff_id"`
-	UserName     string      `json:"userName" db:"user_name"` // Required, unique, max 255 chars. Used for login.
-	Email        string      `json:"email" db:"email"`        // Required, unique, max 255 chars.
-	Password     string      `json:"-" db:"password"`
-	IsActive     bool        `json:"isActive" db:"is_active"`
+// BaseAccount contains the shared fields of account-related types.
+type BaseAccount struct {
+	UserName     string      `json:"userName" db:"user_name"`             // Required, unique, max 255 chars. Used for login.
+	Email        string      `json:"email" db:"email"`                    // Required, unique, max 255 chars.
 	DisplayName  null.String `json:"displayName" db:"display_name"`       // Optional, unique max 255 chars.
 	Department   null.String `json:"department" db:"department"`          // Optional, max 255 chars.
 	GroupMembers int64       `json:"groupMembers" db:"group_memberships"` // Required.
 }
 
-func (a *Account) GenerateID() {
-	a.ID = null.StringFrom(GenerateID())
+// Sanitize removes leading and trailing spaces
+func (a *BaseAccount) Sanitize() {
+	a.Email = strings.TrimSpace(a.Email)
+	a.UserName = strings.TrimSpace(a.UserName)
+	//a.DisplayName = strings.TrimSpace(a.DisplayName)
+	//a.Department = strings.TrimSpace(a.Department)
 }
 
-func (a *Account) GeneratePassword() error {
-	password, err := gorest.RandomHex(4)
-	if err != nil {
-		return err
+func (a BaseAccount) ValidateEmail() *render.ValidationError {
+	return validator.New("email").Required().Max(256).Email().Validate(a.Email)
+}
+
+func (a BaseAccount) ValidateDisplayName() *render.ValidationError {
+	return validator.New("displayName").Max(256).Validate(a.DisplayName.String)
+}
+
+// Validate checks if required fields are valid
+func (a BaseAccount) Validate() *render.ValidationError {
+	ve := a.ValidateEmail()
+	if ve != nil {
+		return ve
 	}
 
-	a.Password = password
+	ve = validator.New("userName").Required().Max(256).Validate(a.UserName)
+	if ve != nil {
+		return ve
+	}
 
-	return nil
+	return a.ValidateDisplayName()
 }
 
-func (a Account) NormalizeName() string {
+// NormalizedName pick a title to name user in email.
+func (a BaseAccount) NormalizeName() string {
 	if a.DisplayName.Valid {
 		return a.DisplayName.String
 	}
@@ -52,51 +58,40 @@ func (a Account) NormalizeName() string {
 	return a.UserName
 }
 
-// Sanitize removes leading and trailing spaces
-func (a *Account) Sanitize() {
-	a.Email = strings.TrimSpace(a.Email)
-	a.UserName = strings.TrimSpace(a.UserName)
-	//a.DisplayName = strings.TrimSpace(a.DisplayName)
-	//a.Department = strings.TrimSpace(a.Department)
+// Account contains essential data of a user.
+// It is used as response data for user authentication.
+// It is also used to create a new user. In this case, password is set to a random string and sent to the Email of this new user. You must make sure the email already works.
+type Account struct {
+	ID null.String `json:"id" db:"staff_id"`
+	BaseAccount
+	IsActive bool `json:"isActive" db:"is_active"`
 }
 
-// Validate checks if required fields are valid
-func (a Account) Validate() *render.ValidationError {
-	ie := validator.New("email").Required().Max(256).Email().Validate(a.Email)
-	if ie != nil {
-		return ie
+// Credentials build a Credentials instance for
+// current account.
+func (a Account) Credentials(pw string) Credentials {
+	return Credentials{
+		ID: a.ID.String,
+		Login: Login{
+			UserName: a.UserName,
+			Password: pw,
+		},
 	}
-
-	ie = validator.New("userName").Required().Max(256).Validate(a.UserName)
-	if ie != nil {
-		return ie
-	}
-
-	return validator.New("displayName").Max(256).Validate(a.DisplayName.String)
 }
 
-func (a Account) SignUpParcel() (postoffice.Parcel, error) {
-	tmpl, err := template.New("verification").Parse(SignupLetter)
-
-	if err != nil {
-		return postoffice.Parcel{}, err
+// SetEmail sets the email field.
+func (a *Account) SetEmail(email string) *render.ValidationError {
+	if a.Email != "" {
+		return &render.ValidationError{
+			Message: "email could only be set once",
+			Field:   "email",
+			Code:    render.CodeAlreadyExists,
+		}
 	}
 
-	var body strings.Builder
-	err = tmpl.Execute(&body, a)
+	a.Email = email
 
-	if err != nil {
-		return postoffice.Parcel{}, err
-	}
-
-	return postoffice.Parcel{
-		FromAddress: "report@ftchinese.com",
-		FromName:    "FT中文网",
-		ToAddress:   a.Email,
-		ToName:      a.NormalizeName(),
-		Subject:     "Welcome",
-		Body:        body.String(),
-	}, nil
+	return nil
 }
 
 func (a Account) PasswordResetParcel(token string) (postoffice.Parcel, error) {
