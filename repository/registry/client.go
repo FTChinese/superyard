6cb7816f71d2f6576a82f569265275e83f33adfb
neefrankie <neefrankie@gmail.com>
2020-03-28 11:22:40 +0800

    refactor

--- repository/registry/client.go
+++ repository/registry/client.go
@@ -1,21 +1,26 @@
 package registry
 
 import (
-	"github.com/jmoiron/sqlx"
-	"github.com/sirupsen/logrus"
 	"gitlab.com/ftchinese/superyard/models/util"
 
 	"gitlab.com/ftchinese/superyard/models/oauth"
 )
 
-// Env wraps db.
-type Env struct {
-	DB *sqlx.DB
-}
+const stmtCreateApp = `
+INSERT INTO oauth.app_registry
+SET app_name = :app_name,
+	slug_name = :slug_name,
+	client_id = UNHEX(:client_id),
+	client_secret = UNHEX(:client_secret),
+	repo_url = :repo_url,
+	description = :description,
+	homepage_url = :home_url,
+	callback_url = :callback_url,
+	created_utc = UTC_TIMESTAMP(),
+	updated_utc = UTC_TIMESTAMP(),
+	owned_by = :owned_by`
 
-var logger = logrus.WithField("package", "repository/access")
-
-// CreateApp inserts a new row into oauth.app_registry table
+// CreateApp registers a new app.
 func (env Env) CreateApp(app oauth.App) error {
 
 	_, err := env.DB.NamedExec(stmtCreateApp, app)
@@ -28,6 +33,10 @@ func (env Env) CreateApp(app oauth.App) error {
 
 	return nil
 }
+
+const stmtListApps = stmtSelectApp + `
+ORDER BY created_utc DESC
+LIMIT ? OFFSET ?`
 
 // ListApps retrieves all apps for next-api with pagination support.
 func (env Env) ListApps(p util.Pagination) ([]oauth.App, error) {
@@ -48,6 +57,10 @@ func (env Env) ListApps(p util.Pagination) ([]oauth.App, error) {
 	return apps, nil
 }
 
+const stmtApp = stmtSelectApp + `
+WHERE client_id = UNHEX(?)
+LIMIT 1`
+
 // RetrieveApp retrieves an ftc app regardless of who owns it.
 // The whole team should be accessible to all apps.
 func (env Env) RetrieveApp(clientID string) (oauth.App, error) {
@@ -64,6 +77,20 @@ func (env Env) RetrieveApp(clientID string) (oauth.App, error) {
 	return app, nil
 }
 
+const stmtUpdateApp = `
+UPDATE oauth.app_registry
+SET app_name = :name,
+	slug_name = :slug,
+	repo_url = :repo_url,
+	description = :description,
+	homepage_url = :home_url,
+	callback_url = :callback_url,
+	updated_utc = UTC_TIMESTAMP()
+WHERE client_id = UNHEX(:client_id)
+	AND owned_by = :owned_by
+	AND is_active = 1
+LIMIT 1`
+
 // UpdateApp allows user to update a ftc app.
 func (env Env) UpdateApp(app oauth.App) error {
 
@@ -78,22 +105,36 @@ func (env Env) UpdateApp(app oauth.App) error {
 	return nil
 }
 
+const stmtRemoveApp = `
+UPDATE oauth.app_registry
+	SET is_active = 0
+WHERE client_id = UNHEX(?)
+	AND is_active = 1
+LIMIT 1`
+
+const stmtRemoveAppKeys = `
+UPDATE oauth.access
+	SET is_active = 0
+WHERE client_id = UNHEX(:client_id)
+	AND usage_type = 'app'`
+
 // RemoveApp deactivate an ftc app.
+// Ony owner can remove his apps.
 // All access tokens belonging to this app should be deactivated.
-func (env Env) RemoveApp(by oauth.AppRemover) error {
+func (env Env) RemoveApp(clientID string) error {
 	tx, err := env.DB.Beginx()
 	if err != nil {
 		logger.WithField("trace", "RemoveApp").Error()
 		return err
 	}
 
-	_, err = tx.NamedExec(stmtRemoveApp, by)
+	_, err = tx.Exec(stmtRemoveApp, clientID)
 	if err != nil {
 		_ = tx.Rollback()
 		logger.WithField("trace", "Env.RemoveApp").Error(err)
 	}
 
-	_, err = tx.NamedExec(stmtRemoveAppKeys, by)
+	_, err = tx.Exec(stmtRemoveAppKeys, clientID)
 	if err != nil {
 		_ = tx.Rollback()
 		logger.WithField("trace", "Env.RemoveApp").Error(err)
