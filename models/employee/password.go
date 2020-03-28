d9df41b232b640bbddfdebcd83478239bd2d4103
neefrankie <neefrankie@gmail.com>
2020-03-28 11:18:34 +0800

    doc

--- models/employee/password.go
+++ models/employee/password.go
@@ -1,10 +1,62 @@
 package employee
 
 import (
+	"github.com/FTChinese/go-rest"
 	"github.com/FTChinese/go-rest/render"
 	"gitlab.com/ftchinese/superyard/models/validator"
 	"strings"
 )
+
+// PasswordReset holds password resetting data.
+// The fields won't all exist at the same time.
+// When requesting a reset email, only `Email`
+// is present.
+// When the link in email is clicked, only `Token`
+// is present.
+// When new password is submitted, `Token` and `Password` will be present.
+type PasswordReset struct {
+	Email    string `json:"email" db:"email"`
+	Token    string `json:"token" db:"token"`
+	Password string `json:"password"`
+}
+
+// GeneratedToken creates a password reset token
+// after user's email is submitted.
+func (r *PasswordReset) GenerateToken() error {
+	token, err := gorest.RandomHex(32)
+	if err != nil {
+		return err
+	}
+
+	r.Token = token
+
+	return nil
+}
+
+// Sanitize removes leading and trailing space of each field
+func (r *PasswordReset) Sanitize() {
+	r.Token = strings.TrimSpace(r.Token)
+	r.Password = strings.TrimSpace(r.Password)
+	r.Token = strings.TrimSpace(r.Token)
+}
+
+func (r PasswordReset) ValidateEmail() *render.ValidationError {
+	return validator.New("email").Required().Email().Validate(r.Email)
+}
+
+func (r PasswordReset) ValidatePass() *render.ValidationError {
+	return validator.New("password").
+		Required().
+		Min(8).
+		Max(256).
+		Validate(r.Password)
+}
+
+// Credentials holds user's identity and password.
+type Credentials struct {
+	ID string `db:"staff_id"`
+	Login
+}
 
 // Password marshals request data for updating password
 type Password struct {
