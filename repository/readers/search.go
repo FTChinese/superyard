59ff0731d0e2dc01993c2b331fad6f87cf6c3dde
neefrankie <neefrankie@gmail.com>
2020-07-03 11:06:14 +0800

    deleted unused files

--- repository/readers/search.go
+++ repository/readers/search.go
@@ -1,95 +1,57 @@
 package readers
 
 import (
-	"strings"
-
+	gorest "github.com/FTChinese/go-rest"
 	"gitlab.com/ftchinese/superyard/models/reader"
-	"gitlab.com/ftchinese/superyard/models/util"
 )
-
-func (env Env) findFtcIDByEmail(email string) (string, error) {
-	var ftcID string
-
-	if err := env.DB.Get(&ftcID, selectFtcIDByEmail, email); err != nil {
-		logger.WithField("trace", "Env.findFtcIDByEmail").Error(err)
-		return "", err
-	}
-
-	return ftcID, nil
-}
 
 // SearchFtcAccount tries to find an FTC account by email.
 // Returns a single account if found, since the email is
 // uniquely constrained.
-func (env Env) SearchFtcAccount(email string) (reader.BaseAccount, error) {
-	ftcID, err := env.findFtcIDByEmail(email)
-	if err != nil {
-		return reader.BaseAccount{}, err
-	}
-
-	a, err := env.retrieveFTCAccount(ftcID)
-	if err != nil {
-		return reader.BaseAccount{}, err
-	}
-
-	return a, nil
-}
-
-// findWxIDs collects all wechat union ids whose nickname contains the
-// parameter `nickname`.
-func (env Env) findWxIDs(nickname string, p util.Pagination) ([]string, error) {
-	var ids []string
+func (env Env) SearchFtcAccount(email string, p gorest.Pagination) ([]reader.FtcWxAccount, error) {
+	var raws = []reader.AccountSchema{}
 
 	err := env.DB.Select(
-		&ids,
-		selectWxIDs,
-		nickname,
+		&raws,
+		reader.StmtSearchFtcByEmail,
+		"%"+email+"%",
 		p.Limit,
 		p.Offset())
-	// NOTE: ErrNoRows won't be thrown if no rows found for statement selecting
-	// multiple rows.
+
 	if err != nil {
-		logger.WithField("trace", "Env.findWxIDs").Error(err)
+		logger.WithField("trace", "Env.SearchFtcAccount").Error(err)
 		return nil, err
 	}
 
-	// An emtpy slice will be returned if nothing found.
-	return ids, nil
-}
-
-// retrieveWxAccounts loads wechat acocunt for the passed in union ids.
-func (env Env) retrieveWxAccounts(ids []string) ([]reader.BaseAccount, error) {
-	// NOTE: JOSN marshal result for the empty array is `[]`
-	// while for `var accounts []reader.BaseAccount` is `null`.
-	var accounts = []reader.BaseAccount{}
-
-	if len(ids) == 0 {
-		return accounts, nil
-	}
-
-	err := env.DB.Select(&accounts, selectWxAccounts, strings.Join(ids, ","))
-	if err != nil {
-		logger.WithField("trace", "Env.retrieveWxAccounts").Error(err)
-		return nil, err
-	}
-
-	for i := range accounts {
-		accounts[i].SetKind()
+	var accounts = make([]reader.FtcWxAccount, 0)
+	for _, raw := range raws {
+		accounts = append(accounts, raw.FtcWxAccount())
 	}
 
 	return accounts, nil
 }
 
 // SearchWxAccounts tries to find out all wechat user with a LIKE statement.
-func (env Env) SearchWxAccounts(nickname string, p util.Pagination) ([]reader.BaseAccount, error) {
-	unionIDs, err := env.findWxIDs(nickname, p)
+func (env Env) SearchWxAccounts(nickname string, p gorest.Pagination) ([]reader.FtcWxAccount, error) {
+	// NOTE: JOSN marshal result for the empty array is `[]`
+	// while for `var rawAccounts []reader.FtcAccount` is `null`.
+	var rawAccounts = []reader.AccountSchema{}
+
+	err := env.DB.Select(
+		&rawAccounts,
+		reader.StmtSearchWxByName,
+		"%"+nickname+"%",
+		p.Limit,
+		p.Offset())
+
 	if err != nil {
+		logger.WithField("trace", "Env.SearchWxAccounts").Error(err)
 		return nil, err
 	}
 
-	accounts, err := env.retrieveWxAccounts(unionIDs)
-	if err != nil {
-		return nil, err
+	accounts := make([]reader.FtcWxAccount, 0)
+	for _, raw := range rawAccounts {
+		accounts = append(accounts, raw.FtcWxAccount())
 	}
 
 	return accounts, nil
