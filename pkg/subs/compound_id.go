9038d2f027a1eb159402838a316c42e9f5ebfaf7
neefrankie <neefrankie@gmail.com>
2020-07-03 10:52:10 +0800

    refactor: rename file

---
+++ pkg/subs/compound_id.go
@@ -0,0 +1,23 @@
+package subs
+
+import "strings"
+
+type CompoundIDs struct {
+	FtcID   string `query:"ftc_id"`
+	UnionID string `query:"union_id"`
+}
+
+// BuildFindInSet produces a value that can be using in FIND_IN_SET(col, value).
+func (c CompoundIDs) BuildFindInSet() string {
+	var ids []string
+
+	if c.FtcID != "" {
+		ids = append(ids, c.FtcID)
+	}
+
+	if c.UnionID != "" {
+		ids = append(ids, c.UnionID)
+	}
+
+	return strings.Join(ids, ",")
+}
