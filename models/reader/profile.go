2147538de3d8f2bda4dfadad520e563815d1a455
neefrankie <neefrankie@gmail.com>
2020-07-03 11:05:35 +0800

    refactor: Profile structure

--- models/reader/profile.go
+++ models/reader/profile.go
@@ -6,27 +6,45 @@ import (
 	"github.com/guregu/null"
 )
 
-// FtcProfile show ftc-only account.
-type FtcProfile struct {
-	ID        string      `json:"id" db:"ftc_id"`
-	UnionID   null.String `json:"unionId" db:"union_id"`
-	StripeID  null.String `json:"stripeId" db:"stripe_id"`
-	Email     string      `json:"email" db:"email"`
-	UserName  null.String `json:"userName" db:"user_name"`
+type Address struct {
+	Country  null.String `json:"country" db:"country"`
+	Province null.String `json:"province" db:"province"`
+	City     null.String `json:"city" db:"city"`
+	District null.String `json:"district" db:"district"`
+	Street   null.String `json:"street" db:"street"`
+	Postcode null.String `json:"postcode" db:"postcode"`
+}
+
+type FtcBaseProfile struct {
 	Mobile    null.String `json:"mobile" db:"mobile"`
-	IsVIP     bool        `json:"vip" db:"is_vip"`
 	Gender    enum.Gender `json:"gender" db:"gender"`
 	LastName  null.String `json:"lastName" db:"last_name"`
 	FirstName null.String `json:"firstName" db:"first_name"`
 	Birthday  chrono.Date `json:"birthday" db:"birthday"`
-	Country   null.String `json:"country" db:"country"`
-	Province  null.String `json:"province" db:"province"`
-	City      null.String `json:"city" db:"city"`
-	District  null.String `json:"district" db:"district"`
-	Street    null.String `json:"street" db:"street"`
-	Postcode  null.String `json:"postcode" db:"postcode"`
+
 	CreatedAt chrono.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt chrono.Time `json:"updatedAt" db:"updated_at"`
+}
+
+// FtcProfile show ftc-only account.
+type FtcProfile struct {
+	FtcAccount
+	FtcBaseProfile
+	Address Address `json:"address"`
+}
+
+type FtcProfileSchema struct {
+	FtcAccount
+	FtcBaseProfile
+	Address
+}
+
+func (s FtcProfileSchema) Build() FtcProfile {
+	return FtcProfile{
+		FtcAccount:     s.FtcAccount,
+		FtcBaseProfile: s.FtcBaseProfile,
+		Address:        s.Address,
+	}
 }
 
 // WxProfile show wx-only account
