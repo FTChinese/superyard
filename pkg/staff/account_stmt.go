535b1dbe8d9d67088187a773ab05cb1216023b92
neefrankie <neefrankie@gmail.com>
2020-07-13 23:31:56 +0800

    refactor: relocate StmtCreateAccount

--- pkg/staff/account_stmt.go
+++ pkg/staff/account_stmt.go
@@ -1,17 +1,5 @@
 package staff
 
-const StmtCreateAccount = `
-INSERT INTO backyard.staff
-SET staff_id = :staff_id,
-	user_name = :user_name,
-	email = :email,
-	password = UNHEX(MD5(:password)),
-	display_name = :display_name,
-	department = :department,
-	group_memberships = :group_memberships,
-	created_utc = UTC_TIMESTAMP(),
-	updated_utc = UTC_TIMESTAMP()`
-
 const StmtAccountCols = `
 SELECT s.staff_id 		AS staff_id,
 	s.user_name 		AS user_name,
