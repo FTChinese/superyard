544b797359fd43c0a32f75f1ab6174899bd7d0b9
neefrankie <neefrankie@gmail.com>
2020-07-13 20:42:54 +0800

    refactor: delete login.go

--- pkg/staff/login.go
+++
@@ -1,7 +0,0 @@
-package staff
-
-// Login specifies the the fields used for authentication
-type Login struct {
-	UserName string `json:"userName" db:"user_name"`
-	Password string `json:"password" db:"password"`
-}
