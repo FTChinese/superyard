031528c3ecf4cfee8e81b607166491d91a3e6fca
weiguo.ni <neefrankie@gmail.com>
2020-07-13 15:42:58 +0800

    feat: sql for login

---
+++ pkg/staff/login_stmt.go
@@ -0,0 +1,13 @@
+package staff
+
+const StmtLogin = StmtAccountCols + `
+FROM backyard.staff AS s
+WHERE (s.user_name, .s.password) = (?, UNHEX(MD5(?)))
+	AND s.is_active = 1`
+
+const StmtUpdateLastLogin = `
+UPDATE backyard.staff
+SET last_login_utc = UTC_TIMESTAMP(),
+	last_login_ip = IFNULL(INET6_ATON(?), last_login_ip)
+WHERE user_name = ?
+LIMIT 1`
