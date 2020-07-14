d129d7d51de8b932123f4e03972390656ef1ae9e
weiguo.ni <neefrankie@gmail.com>
2020-07-14 17:50:34 +0800

    feat: build confirmation order and membership

--- pkg/subs/member_snapshot.go
+++ pkg/subs/member_snapshot.go
@@ -3,6 +3,7 @@ package subs
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/FTChinese/go-rest/enum"
+	"github.com/guregu/null"
 )
 
 // MemberSnapshot saves a membership's status prior to
@@ -11,5 +12,6 @@ type MemberSnapshot struct {
 	ID         string              `db:"snapshot_id"`
 	Reason     enum.SnapshotReason `db:"reason"`
 	CreatedUTC chrono.Time         `db:"created_utc"`
+	OrderID    null.String         `db:"order_id"` // Only exists when user is performing renewal or upgrading.
 	Membership
 }
