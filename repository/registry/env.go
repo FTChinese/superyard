15f508f22a094b52c6e27da8f818da95406ae471
weiguo.ni <neefrankie@gmail.com>
2020-07-14 16:43:03 +0800

    refactor: removed sql

--- repository/registry/env.go
+++ repository/registry/env.go
@@ -11,17 +11,3 @@ type Env struct {
 }
 
 var logger = logrus.WithField("package", "repository/registry")
-
-const stmtSelectToken = `
-SELECT k.id AS id,
-	LOWER(HEX(k.access_token)) AS token,
-	k.is_active AS is_active,
-	k.expires_in AS expires_in,
-	k.usage_type AS usage_type,
-	k.client_id AS client_id,
-	k.description AS description,
-	k.created_by AS created_by,
-	k.created_utc AS created_at,
-	k.updated_utc AS updated_at,
-	k.last_used_utc AS last_used_at
-FROM oauth.access AS k`
