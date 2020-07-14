91553b0fd661964a079466787059735f6f33940e
weiguo.ni <neefrankie@gmail.com>
2020-07-14 11:22:35 +0800

    test: JWT creation and parsing

--- pkg/staff/jwt_test.go
+++ pkg/staff/jwt_test.go
@@ -1,44 +1,21 @@
 package staff
 
-import (
-	"github.com/dgrijalva/jwt-go"
-	"testing"
-	"time"
-)
+import "testing"
 
-func TestJWTSign(t *testing.T) {
-	mySigningKey := []byte("AllYourBase")
+func TestNewPassportBearer(t *testing.T) {
+	key := []byte("signing_key")
 
-	claims := jwt.StandardClaims{
-		ExpiresAt: time.Now().Unix() + 86400*7,
-		Id:        "stf_1234567890",
-		IssuedAt:  time.Now().Unix(),
-		Issuer:    "ftc_superyard",
-		NotBefore: 0,
-		Subject:   "",
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	ss, err := token.SignedString(mySigningKey)
-
+	pp, err := NewPassportBearer(mockAccount, key)
 	if err != nil {
 		t.Error(err)
 	}
 
-	t.Log(ss)
+	t.Logf("PassportBearer: %+v", pp)
 
-	token, err = jwt.ParseWithClaims(ss, &jwt.StandardClaims{}, func(token *jwt.Token) (i interface{}, err error) {
-		return mySigningKey, nil
-	})
-
+	claims, err := ParsePassportClaims(pp.Token, key)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if claims, ok := token.Claims.(*jwt.StandardClaims); ok {
-		t.Logf("Is token valid: %t", claims.Valid() == nil)
-	} else {
-		t.Error(err)
-	}
+	t.Logf("Parsed PassportClaimns: %+v", claims)
 }
