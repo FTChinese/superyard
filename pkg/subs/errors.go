760745f616a34797aa609509f1c2cccc2d0b729c
weiguo.ni <neefrankie@gmail.com>
2020-07-15 10:34:36 +0800

    feat: add ErrValidNonAliOrWxPay

--- pkg/subs/errors.go
+++ pkg/subs/errors.go
@@ -3,6 +3,7 @@ package subs
 import "errors"
 
 var (
-	ErrAlreadyConfirmed = errors.New("order already confirmed")
-	ErrAlreadyUpgraded  = errors.New("already subscribed to premium edition")
+	ErrAlreadyConfirmed   = errors.New("order already confirmed")
+	ErrAlreadyUpgraded    = errors.New("already subscribed to premium edition")
+	ErrValidNonAliOrWxPay = errors.New("only membership created by wxpay or alipay could be confirmed")
 )
