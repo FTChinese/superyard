4118e20048f406c93f78c29a076fc8393f89b63b
weiguo.ni <neefrankie@gmail.com>
2020-07-15 16:33:06 +0800

    refactor: split Article fields

--- pkg/wiki/article.go
+++ pkg/wiki/article.go
@@ -5,17 +5,50 @@ import (
 	"github.com/guregu/null"
 )
 
-type BaseArticle struct {
+// ArticleMeta contains metadata of an article.
+type ArticleMeta struct {
 	ID         int64       `json:"id" db:"id"`
-	Title      string      `json:"title" db:"title"`
 	Author     string      `json:"author" db:"author"`
-	Summary    null.String `json:"summary" db:"summary"`
-	Keyword    null.String `json:"keyword" db:"keyword"`
 	CreatedUTC chrono.Time `json:"createdUtc" db:"created_utc"`
 	UpdatedUTC chrono.Time `json:"updatedUtc" db:"updated_utc"`
 }
 
+func NewArticleMeta(author string) ArticleMeta {
+	return ArticleMeta{
+		ID:         0,
+		Author:     author,
+		CreatedUTC: chrono.TimeNow(),
+		UpdatedUTC: chrono.TimeNow(),
+	}
+}
+
+// ArticleOverview is used as an item of a list of articles.
+type ArticleTeaser struct {
+	ArticleMeta
+	Title   string      `json:"title" db:"title"`
+	Summary null.String `json:"summary" db:"summary"`
+	Keyword null.String `json:"keyword" db:"keyword"`
+}
+
+// Article contains the full data of an article.
 type Article struct {
-	BaseArticle
+	ArticleTeaser
 	Body string `json:"body" db:"body"`
+}
+
+// NewArticle creates a new Article based on user input.
+func NewArticle(input Article, author string) Article {
+	input.ArticleMeta = NewArticleMeta(author)
+
+	return input
+}
+
+// Update an existing article. Since the request body
+// does not contain the article's id, you have to get it
+// from the path parameter.
+func (a Article) Update(id int64) Article {
+	a.ID = id
+	a.UpdatedUTC = chrono.TimeNow()
+
+	return a
 }
