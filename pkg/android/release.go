d95706d10dd91e294eb9c5d183f55eec3a4c589e
weiguo.ni <neefrankie@gmail.com>
2020-07-17 16:39:57 +0800

    refactor: add ReleaseInput type

--- pkg/android/release.go
+++ pkg/android/release.go
@@ -8,27 +8,31 @@ import (
 	"strings"
 )
 
-type Release struct {
-	VersionName string      `json:"versionName" db:"version_name"` // Required.
-	VersionCode int64       `json:"versionCode" db:"version_code"` // Required
-	Body        null.String `json:"body" db:"body"`                // Optional
-	ApkURL      string      `json:"apkUrl" db:"apk_url"`           // Required.
-	CreatedAt   chrono.Time `json:"createdAt" db:"created_at"`
-	UpdatedAt   chrono.Time `json:"updatedAt" db:"updated_at"`
+// ReleaseInput contains the fields required to create or update a release.
+// For creation, VersionName + VersionCode + ApkURL are required.
+// For update, only ApkURL is required.
+type ReleaseInput struct {
+	// Required only when creating a new release. Ignore it when updating since the version name is
+	// acquired from path parameter.
+	VersionName string `json:"versionName" db:"version_name"`
+	// Required only when creating a new release. This cannot be changed upon updating.
+	VersionCode int64       `json:"versionCode" db:"version_code"`
+	Body        null.String `json:"body" db:"body"`      // Optional
+	ApkURL      string      `json:"apkUrl" db:"apk_url"` // Required.
 }
 
-func (r *Release) Validate() *render.ValidationError {
-	r.VersionName = strings.TrimSpace(r.VersionName)
+func (r *ReleaseInput) ValidateUpdate() *render.ValidationError {
 	r.Body.String = strings.TrimSpace(r.Body.String)
 	r.ApkURL = strings.TrimSpace(r.ApkURL)
 
-	if r.VersionCode < 1 {
-		return &render.ValidationError{
-			Message: "version code must be larger than 0",
-			Field:   "versionCode",
-			Code:    render.CodeInvalid,
-		}
-	}
+	return validator.New("apkUrl").
+		Required().
+		URL().
+		Validate(r.ApkURL)
+}
+
+func (r *ReleaseInput) ValidateCreation() *render.ValidationError {
+	r.VersionName = strings.TrimSpace(r.VersionName)
 
 	ie := validator.New("versionName").
 		Required().
@@ -38,5 +42,27 @@ func (r *Release) Validate() *render.ValidationError {
 		return ie
 	}
 
-	return validator.New("apkUrl").Required().URL().Validate(r.ApkURL)
+	if r.VersionCode < 1 {
+		return &render.ValidationError{
+			Message: "version code must be larger than 0",
+			Field:   "versionCode",
+			Code:    render.CodeInvalid,
+		}
+	}
+
+	return r.ValidateUpdate()
+}
+
+type Release struct {
+	ReleaseInput
+	CreatedAt chrono.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt chrono.Time `json:"updatedAt" db:"updated_at"`
+}
+
+func NewRelease(input ReleaseInput) Release {
+	return Release{
+		ReleaseInput: input,
+		CreatedAt:    chrono.TimeNow(),
+		UpdatedAt:    chrono.TimeNow(),
+	}
 }
