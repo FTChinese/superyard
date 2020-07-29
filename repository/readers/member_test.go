82ab744f8a285bdf80319735c6baafc483c2c8d3
weiguo.ni <neefrankie@gmail.com>
2020-07-29 10:21:34 +0800

    test: retrieve membership from different columns

--- repository/readers/member_test.go
+++ repository/readers/member_test.go
@@ -2,6 +2,9 @@ package readers
 
 import (
 	"github.com/FTChinese/go-rest/chrono"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/ftchinese/superyard/pkg/reader"
 	"gitlab.com/ftchinese/superyard/pkg/subs"
 	"gitlab.com/ftchinese/superyard/test"
 	"testing"
@@ -66,6 +69,60 @@ func TestEnv_UpdateMember(t *testing.T) {
 			if err := env.UpdateMember(tt.args.m); (err != nil) != tt.wantErr {
 				t.Errorf("UpdateMember() error = %v, wantErr %v", err, tt.wantErr)
 			}
+		})
+	}
+}
+
+func TestEnv_FindMemberForOrder(t *testing.T) {
+	p := test.NewPersona().
+		SetAccountKind(reader.AccountKindLinked)
+	repo := test.NewRepo()
+
+	repo.MustCreateMembership(p.Membership())
+
+	type fields struct {
+		DB *sqlx.DB
+	}
+	type args struct {
+		ftcOrUnionID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:   "By ftc id",
+			fields: fields{DB: test.DBX},
+			args: args{
+				ftcOrUnionID: p.FtcID,
+			},
+			wantErr: false,
+		},
+		{
+			name: "By union id",
+			fields: fields{
+				DB: test.DBX,
+			},
+			args: args{
+				ftcOrUnionID: p.UnionID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				DB: tt.fields.DB,
+			}
+			got, err := env.FindMemberForOrder(tt.args.ftcOrUnionID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindMemberForOrder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			assert.NotZero(t, got)
 		})
 	}
 }
