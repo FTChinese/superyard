246926cf3018bb6d852de244bd7f730562912f58
neefrankie <neefrankie@gmail.com>
2020-08-13 23:20:58 +0800

    fix: syntax error

--- pkg/paywall/discount_stmt.go
+++ pkg/paywall/discount_stmt.go
@@ -3,9 +3,9 @@ package paywall
 // StmtCreateDiscount creates a new discount for a plan.
 const StmtCreateDiscount = `
 INSERT INTO subs.discount
-SET id = :discount_id
-    plan_id = :plan_id
-    price_off = :price_off
+SET id = :discount_id,
+    plan_id = :plan_id,
+    price_off = :price_off,
     start_utc = :start_utc,
     end_utc = :end_utc,
     created_utc = :created_utc,
@@ -16,5 +16,11 @@ SET id = :discount_id
 const StmtApplyDiscount = `
 UPDATE subs.plan
 SET discount_id = :discount_id
+WHERE id = :plan_id
+LIMIT 1`
+
+const StmtDropDiscount = `
+UPDATE subs.plan
+SET discount_id = NULL
 WHERE id = :plan_id
 LIMIT 1`
