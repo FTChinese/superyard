aeaeaa6eb180fbc68945db00272ff2cf1fb64090
neefrankie <neefrankie@gmail.com>
2020-08-13 23:25:49 +0800

    feat: common columns for product

--- pkg/paywall/product_stmt.go
+++ pkg/paywall/product_stmt.go
@@ -11,18 +11,26 @@ SET id = :product_id,
     created_utc = UTC_TIMESTAMP(),
     updated_utc = UTC_TIMESTAMP()`
 
+// pp refer to paywall_product table.
+const colProduct = `
+SELECT prod.id AS product_id,
+	prod.tier,
+	prod.heading,
+	prod.description,
+	prod.small_print,
+	pp.product_id IS NOT NULL AS is_active,
+	prod.created_by,
+	prod.created_utc,
+	prod.updated_utc
+`
+
 // StmtProduct retrieves a single product by id.
-// This is used when modify an existing product.
-const StmtProduct = `
-SELECT id AS product_id,
-    tier,
-    heading,
-    description,
-    small_print,
-    created_by,
-    created_utc,
-    updated_utc
-FROM subs.product
+// This is used when modify an existing product,
+// or showing the details of a product.
+const StmtProduct = colProduct + `
+FROM subs.product AS prod
+	LEFT JOIN subs.paywall_product AS pp
+	ON prod.id = pp.product_id
 WHERE id = ?
 LIMIT 1`
 
@@ -37,7 +45,7 @@ LIMIT 1`
 
 const planJSONSchema = `
 'id', id,
-'productId', product_id
+'productId', product_id,
 'price', price,
 'tier', tier,
 'cycle', cycle,
@@ -48,24 +56,29 @@ const planJSONSchema = `
 const groupPlansOfProduct = `
 SELECT product_id,
 	JSON_ARRAYAGG(JSON_OBJECT(` + planJSONSchema + `)) AS basePlans
-FROM plan
+FROM subs.plan
 GROUP BY product_id`
 
-const colProduct = `
-SELECT prod.id,
-	prod.tier,
-	prod.heading,
-	prod.description,
-	prod.small_print,
-	prod.created_by,
-	prod.created_utc,
-	prod.updated_utc
-`
-
 // StmtListPricedProducts retrieves a list of product.
-const StmtListPricedProducts = colProduct + `
-	IFNULL(plan.basePlans, JSON_ARRAY())
+const StmtListPricedProducts = colProduct + `,
+	IFNULL(plan.basePlans, JSON_ARRAY()) AS plans
 FROM subs.product AS prod
   	LEFT JOIN (` + groupPlansOfProduct + `) AS plan
 	ON prod.id = plan.product_id
+	LEFT JOIN subs.paywall_product AS pp
+	ON prod.id = pp.product_id
 ORDER BY prod.tier ASC`
+
+const StmtActivateProduct = `
+INSERT INTO subs.paywall_product
+SET product_id = :product_id,
+	tier = :tier
+ON DUPLICATE KEY UPDATE
+	product_id = :product_id`
+
+const StmtHasActivePlan = `
+SELECT EXISTS (
+	SELECT *
+	FROM subs.product_active_plans
+	WHERE product_id = ?
+) AS has_plan`
