607ecb3705ca2c68f8bca47177a4d3bdb482c425
neefrankie <neefrankie@gmail.com>
2020-08-13 23:25:08 +0800

    feat: add ActivateProduct

--- repository/products/product.go
+++ repository/products/product.go
@@ -1,36 +1,18 @@
 package products
 
-import "github.com/FTChinese/superyard/pkg/paywall"
-
-func (env Env) ListPricedProducts() ([]paywall.PricedProduct, error) {
-	schema := make([]paywall.PricedProductSchema, 0)
-	products := make([]paywall.PricedProduct, 0)
-
-	err := env.db.Select(&schema, paywall.StmtListPricedProducts)
-
-	if err != nil {
-		return products, err
-	}
-
-	for _, v := range schema {
-		prod, err := v.PricedProduct()
-		if err != nil {
-			return products, err
-		}
-
-		products = append(products, prod)
-	}
-
-	return products, nil
-}
+import (
+	"github.com/FTChinese/superyard/pkg/paywall"
+)
 
 func (env Env) CreatePricedProduct(p paywall.PricedProduct) error {
+
 	tx, err := env.db.Beginx()
 	if err != nil {
 		return err
 	}
 
-	_, err = tx.NamedExec(paywall.StmtCreateProduct, p)
+	logger.Info("Creating product...")
+	_, err = tx.NamedExec(paywall.StmtCreateProduct, p.Product)
 
 	if err != nil {
 		getLogger("CreatePricedProduct").Error(err)
@@ -38,11 +20,16 @@ func (env Env) CreatePricedProduct(p paywall.PricedProduct) error {
 	}
 
 	if len(p.Plans) > 0 {
+		logger.Infof("Creating %d plans", len(p.Plans))
+
 		for _, v := range p.Plans {
 			_, err := tx.NamedExec(paywall.StmtCreatePlan, v)
-			return err
+			if err != nil {
+				return err
+			}
 		}
 	}
+
 	if err := tx.Commit(); err != nil {
 		return err
 	}
@@ -71,4 +58,38 @@ func (env Env) UpdateProduct(prod paywall.Product) error {
 	}
 
 	return nil
+}
+
+func (env Env) ActivateProduct(prod paywall.Product) error {
+	_, err := env.db.NamedExec(paywall.StmtActivateProduct, prod)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ListPricedProducts list all products, with each product's plans attached without discount.
+// A product's plans are retrieve using JSON_ARRAYAGG.
+func (env Env) ListPricedProducts() ([]paywall.PricedProduct, error) {
+	schema := make([]paywall.PricedProductSchema, 0)
+	products := make([]paywall.PricedProduct, 0)
+
+	err := env.db.Select(&schema, paywall.StmtListPricedProducts)
+
+	if err != nil {
+		return products, err
+	}
+
+	for _, v := range schema {
+		prod, err := v.PricedProduct()
+		if err != nil {
+			return products, err
+		}
+
+		products = append(products, prod)
+	}
+
+	return products, nil
 }
