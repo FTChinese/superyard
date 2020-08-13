e3a62dd871b09f5c0fe025e664fb3e6907a4bcd9
neefrankie <neefrankie@gmail.com>
2020-08-13 23:23:23 +0800

    feat: add ProductHasActivePlan

--- repository/products/plan.go
+++ repository/products/plan.go
@@ -1,6 +1,9 @@
 package products
 
-import "github.com/FTChinese/superyard/pkg/paywall"
+import (
+	"database/sql"
+	"github.com/FTChinese/superyard/pkg/paywall"
+)
 
 func (env Env) CreatePlan(p paywall.Plan) error {
 	_, err := env.db.NamedExec(paywall.StmtCreatePlan, p)
@@ -12,6 +15,8 @@ func (env Env) CreatePlan(p paywall.Plan) error {
 	return nil
 }
 
+// LoadPlan retrieves a plan. It contains only columns from the plan table.
+// Not discount info is included.
 func (env Env) LoadPlan(id string) (paywall.Plan, error) {
 	var plan paywall.Plan
 
@@ -24,6 +29,12 @@ func (env Env) LoadPlan(id string) (paywall.Plan, error) {
 	return plan, nil
 }
 
+// ActivatePlan sets a plan as active under a product.
+// By active we mean this plan will be presented on paywall
+// when this product is put on paywall.
+// A product could have as many plans linked as you like,
+// but only two (for product tier standard) or one (for premium)
+// set as active.
 func (env Env) ActivatePlan(plan paywall.Plan) error {
 	_, err := env.db.NamedExec(paywall.StmtActivatePlan, plan)
 	if err != nil {
@@ -33,11 +44,27 @@ func (env Env) ActivatePlan(plan paywall.Plan) error {
 	return nil
 }
 
+// ProductHasActivePlan checks if a product has any active plans set for it.
+// If a product does not have any plans, we should disallow
+// it being put on paywall.
+func (env Env) ProductHasActivePlan(productID string) (bool, error) {
+	var ok bool
+	err := env.db.Get(&ok, paywall.StmtHasActivePlan, productID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return ok, nil
+}
+
 func (env Env) ListPlansOfProduct(id string) ([]paywall.DiscountedPlan, error) {
 	schemas := make([]paywall.DiscountedPlanSchema, 0)
 	dPlans := make([]paywall.DiscountedPlan, 0)
 
-	err := env.db.Select(&schemas, paywall.StmtPlansOfProduct)
+	err := env.db.Select(&schemas, paywall.StmtPlansOfProduct, id)
 
 	if err != nil {
 		return dPlans, err
