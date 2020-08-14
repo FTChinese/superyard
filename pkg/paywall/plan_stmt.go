0f50c26985bdb61e2ca3bcb765f2d60a15fbb0f6
weiguo.ni <neefrankie@gmail.com>
2020-08-14 16:28:48 +0800

    refactor: join active plans table

--- pkg/paywall/plan_stmt.go
+++ pkg/paywall/plan_stmt.go
@@ -18,13 +18,15 @@ SELECT p.id AS plan_id,
 	p.tier,
 	p.cycle,
 	p.description,
-	
+	a.plan_id IS NOT NULL AS is_active,
 	p.created_utc,
 	p.created_by`
 
 // StmtPlan selects a single plan.
 const StmtPlan = colPlan + `
 FROM subs_product.plan AS p
+	LEFT JOIN subs_product.product_active_plans AS a
+	ON p.id = a.plan_id
 WHERE p.id = ?
 LIMIT 1`
 
@@ -38,6 +40,7 @@ ON DUPLICATE KEY UPDATE
 
 const colDiscountedPlan = colPlan + `,
 d.id AS discount_id,
+d.plan_id AS discounted_plan_id,
 d.price_off,
 d.percent,
 d.start_utc,
@@ -45,8 +48,7 @@ d.end_utc
 `
 
 // StmtPlansOfProduct selects all plans under a product.
-const StmtPlansOfProduct = colDiscountedPlan + `,
-	a.plan_id IS NOT NULL AS is_active
+const StmtPlansOfProduct = colDiscountedPlan + `
 FROM subs_product.plan AS p
     LEFT JOIN subs_product.discount AS d
 	ON p.discount_id = d.id
