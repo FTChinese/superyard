93e687e8226bf735612dee247bff634b68be1f8b
neefrankie <neefrankie@gmail.com>
2020-08-27 20:44:51 +0800

    test

--- repository/products/paywall_test.go
+++ repository/products/paywall_test.go
@@ -115,3 +115,41 @@ func TestEnv_retrievePaywallPlans(t *testing.T) {
 		})
 	}
 }
+
+func TestEnv_LoadPaywall(t *testing.T) {
+	type fields struct {
+		db *sqlx.DB
+	}
+	type args struct {
+		id int64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Load paywall",
+			fields: fields{
+				db: test.DBX,
+			},
+			args:    args{id: 1},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db: tt.fields.db,
+			}
+			got, err := env.LoadPaywall(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LoadPaywall() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			t.Logf("%+v", got)
+		})
+	}
+}
