f20eaef13d4530c88905db0a3ee02c8f708024ce
neefrankie <neefrankie@gmail.com>
2020-08-30 23:35:40 +0800

    refactor: renaming

--- repository/readers/sandbox.go
+++ repository/readers/sandbox.go
@@ -1,8 +1,10 @@
 package readers
 
-import "github.com/FTChinese/superyard/pkg/reader"
+import (
+	"github.com/FTChinese/superyard/pkg/reader"
+)
 
-func (env Env) CreateSandbox(account reader.SandboxAccount) error {
+func (env Env) CreateSandboxUser(account reader.SandboxUser) error {
 	tx, err := env.DB.Beginx()
 	if err != nil {
 		return err
@@ -14,7 +16,7 @@ func (env Env) CreateSandbox(account reader.SandboxAccount) error {
 		return err
 	}
 
-	_, err = tx.NamedExec(reader.StmtCreateAccount, account)
+	_, err = tx.NamedExec(reader.StmtCreateSandboxUser, account)
 	if err != nil {
 		_ = tx.Rollback()
 		return err
@@ -27,17 +29,66 @@ func (env Env) CreateSandbox(account reader.SandboxAccount) error {
 	return nil
 }
 
-func (env Env) RetrieveSandboxReader(id string) (reader.SandboxAccount, error) {
-	var a reader.SandboxAccount
-	err := env.DB.Get(&a, reader.StmtSandboxAccount, id)
+func (env Env) ListSandboxUsers() ([]reader.SandboxUser, error) {
+	var accounts = make([]reader.SandboxUser, 0)
+	if err := env.DB.Select(&accounts, reader.StmtListSandboxUsers); err != nil {
+		return nil, err
+	}
+
+	return accounts, nil
+}
+
+func (env Env) sandboxUserInfo(ftcId string) (reader.SandboxUser, error) {
+	var a reader.SandboxUser
+	err := env.DB.Get(&a, reader.StmtSandboxUser, ftcId)
 	if err != nil {
-		return reader.SandboxAccount{}, err
+		return reader.SandboxUser{}, err
 	}
 
 	return a, nil
 }
 
-func (env Env) SandboxAccountExists(id string) (bool, error) {
+type sandboxUserResult struct {
+	value reader.SandboxUser
+	err   error
+}
+
+func (env Env) asyncSandboxUser(ftcID string) <-chan sandboxUserResult {
+	c := make(chan sandboxUserResult)
+
+	go func() {
+		defer close(c)
+		s, err := env.sandboxUserInfo(ftcID)
+
+		c <- sandboxUserResult{
+			value: s,
+			err:   err,
+		}
+	}()
+
+	return c
+}
+
+func (env Env) LoadSandboxAccount(ftcID string) (reader.SandboxAccount, error) {
+	sChan, mChan := env.asyncSandboxUser(ftcID), env.asyncMembership(ftcID)
+
+	sResult, mResult := <-sChan, <-mChan
+
+	if sResult.err != nil {
+		return reader.SandboxAccount{}, sResult.err
+	}
+
+	if mResult.err != nil {
+		return reader.SandboxAccount{}, mResult.err
+	}
+
+	return reader.SandboxAccount{
+		SandboxUser: sResult.value,
+		Membership:  mResult.value,
+	}, nil
+}
+
+func (env Env) SandboxUserExists(id string) (bool, error) {
 	var found bool
 	err := env.DB.Get(&found, reader.StmtSandboxExists, id)
 	if err != nil {
