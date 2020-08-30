3f564c13488224eccdf8a8f3569d688830c94775
neefrankie <neefrankie@gmail.com>
2020-08-30 23:34:06 +0800

    refactor: merge MemberRouter to ReaderRouter

--- controller/membership.go
+++ controller/membership.go
@@ -3,56 +3,51 @@ package controller
 import (
 	"github.com/FTChinese/go-rest/render"
 	"github.com/FTChinese/superyard/pkg/reader"
-	"github.com/FTChinese/superyard/repository/readers"
-	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
-type MemberRouter struct {
-	env readers.Env
-}
-
-func NewMemberRouter(db *sqlx.DB) MemberRouter {
-	return MemberRouter{
-		env: readers.Env{DB: db},
-	}
-}
-
 // CreateMember creates a membership for an account.
-// Input:
-// {
-//	compoundId: string,
-//	ftcId?: string,
-//	unionId?: string,
-//	tier: 'standard' | 'premium',
-//	cycle: 'year' | 'month',
-//	expireDate: string,
-//	payMethod: string,
-// }
+//
+// Input: reader.MemberInput
+// expireDate: string;
+// payMethod: string;
+// ftcPlanId: string;
 // ftcId and unionId cannot be both empty.
-func (router MemberRouter) CreateMember(c echo.Context) error {
-
-	var m reader.Membership
-	if err := c.Bind(&m); err != nil {
+func (router ReaderRouter) CreateMember(c echo.Context) error {
+	var input reader.MemberInput
+	if err := c.Bind(&input); err != nil {
 		return render.NewBadRequest(err.Error())
 	}
-
-	if ve := m.ValidateCreate(); ve != nil {
+	if ve := input.Validate(); ve != nil {
 		return render.NewUnprocessable(ve)
 	}
 
-	if err := router.env.CreateMember(m); err != nil {
+	// Find the account for which the membership is created.
+	a, err := router.readerRepo.FtcAccount(input.CompoundID)
+	if err != nil {
+		return render.NewDBError(err)
+	}
+
+	// Find the plan using the ftcPlanId field.
+	plan, err := router.productsRepo.LoadPlan(input.FtcPlanID.String)
+	if err != nil {
+		return render.NewDBError(err)
+	}
+	m := input.NewMembership(a, plan)
+
+	if err := router.readerRepo.CreateMember(m); err != nil {
 		return render.NewDBError(err)
 	}
 
 	return c.NoContent(http.StatusNoContent)
 }
 
-func (router MemberRouter) LoadMember(c echo.Context) error {
+// LoadMember retrieves membership by either ftc uuid of wechat union id.
+func (router ReaderRouter) LoadMember(c echo.Context) error {
 	id := c.Param("id")
 
-	m, err := router.env.RetrieveMember(id)
+	m, err := router.readerRepo.RetrieveMember(id)
 	if err != nil {
 		return render.NewDBError(err)
 	}
@@ -61,46 +56,40 @@ func (router MemberRouter) LoadMember(c echo.Context) error {
 }
 
 // UpdateMember modifies an existing membership.
-// Input:
-// {
-//	tier: 'standard' | 'premium',
-//	cycle: 'year' | 'month',
-//	expireDate: string,
-//	payMethod: string,
-// }
-func (router MemberRouter) UpdateMember(c echo.Context) error {
+// Input: reader.MemberInput
+// expireDate: string;
+// payMethod: string;
+// ftcPlanId: string;
+func (router ReaderRouter) UpdateMember(c echo.Context) error {
 	claims := getPassportClaims(c)
 	id := c.Param("id")
 
-	var m reader.Membership
-	if err := c.Bind(&m); err != nil {
+	var input reader.MemberInput
+	if err := c.Bind(&input); err != nil {
 		return render.NewBadRequest(err.Error())
 	}
-	m.CompoundID = id
+	input.CompoundID = id
 
-	if ve := m.Validate(); ve != nil {
+	if ve := input.Validate(); ve != nil {
 		return render.NewUnprocessable(ve)
 	}
 
-	if err := router.env.UpdateMember(m, claims.Username); err != nil {
-		return render.NewDBError(err)
-	}
-
-	return c.NoContent(http.StatusNoContent)
-}
-
-// FindMemberForOrder tries to find membership by an order's compound id.
-// An order's compound id could be either ftc id or union id.
-// We cannot be sure which kind it is since there are not enough data to
-// distinguish it.
-func (router MemberRouter) FindMemberForOrder(c echo.Context) error {
-	id := c.Param("id")
-
-	m, err := router.env.FindMemberForOrder(id)
-
+	plan, err := router.productsRepo.LoadPlan(input.FtcPlanID.String)
 	if err != nil {
 		return render.NewDBError(err)
 	}
 
-	return c.JSON(http.StatusOK, m)
+	result, err := router.readerRepo.UpdateMember(input, plan)
+	if err != nil {
+		return render.NewDBError(err)
+	}
+
+	go func() {
+		_ = router.readerRepo.SnapshotMember(
+			result.Snapshot.
+				WithCreator(claims.Username),
+		)
+	}()
+
+	return c.JSON(http.StatusOK, result.Membership)
 }
