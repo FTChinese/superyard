bcc7b377f16a5ce04e1b5f7389f6d681558dd23b
weiguo.ni <neefrankie@gmail.com>
2020-09-03 11:16:20 +0800

    fix: wiki update missing where clause

--- pkg/wiki/article_stmt.go
+++ pkg/wiki/article_stmt.go
@@ -31,7 +31,9 @@ SET title = :title,
 	summary = :summary,
 	body = :body,
 	keyword = :keyword,
-	updated_utc = UTC_TIMESTAMP()`
+	updated_utc = UTC_TIMESTAMP()
+WHERE id = :id
+LIMIT 1`
 
 const StmtArticle = articleCols + `
 WHERE id = ?
