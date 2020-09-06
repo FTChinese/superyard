876e4bfc55135a1dad01b7fdeb4334d0e165bfd0
neefrankie <neefrankie@gmail.com>
2020-09-06 19:23:53 +0800

    refactor: client for subscription api

--- repository/subsapi/client.go
+++ repository/subsapi/client.go
@@ -2,7 +2,6 @@ package subsapi
 
 import (
 	"github.com/FTChinese/superyard/pkg/config"
-	"log"
 	"net/http"
 )
 
@@ -29,19 +28,4 @@ func NewClient(debug bool) Client {
 		key:     key,
 		baseURL: baseURL,
 	}
-}
-
-func (c Client) RefreshPaywall() (*http.Response, error) {
-	url := c.baseURL + "/paywall/__refresh"
-
-	log.Printf("Refreshing paywall data at %s", url)
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+c.key)
-
-	return httpClient.Do(req)
 }
