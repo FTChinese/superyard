6800a4bc3192101621da426fe8c03d033e1be0c7
weiguo.ni <neefrankie@gmail.com>
2020-09-15 09:54:47 +0800

    test

--- internal/repository/readers/member_test.go
+++ internal/repository/readers/member_test.go
@@ -1,1 +1,202 @@
 package readers
+
+import (
+	"github.com/FTChinese/superyard/faker"
+	"github.com/FTChinese/superyard/pkg/subs"
+	"github.com/FTChinese/superyard/test"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
+	"testing"
+)
+
+func TestEnv_MemberByCompoundID(t *testing.T) {
+	p := test.NewPersona()
+
+	test.NewRepo().MustCreateMembership(p.Membership())
+
+	type fields struct {
+		db     *sqlx.DB
+		logger *zap.Logger
+	}
+	type args struct {
+		compoundID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Retrieve by compound id",
+			fields: fields{
+				db:     test.DBX,
+				logger: zaptest.NewLogger(t),
+			},
+			args: args{
+				compoundID: p.FtcID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				logger: tt.fields.logger,
+			}
+			got, err := env.MemberByCompoundID(tt.args.compoundID)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
+
+func TestEnv_CreateFtcMember(t *testing.T) {
+	p := test.NewPersona()
+
+	type fields struct {
+		db     *sqlx.DB
+		logger *zap.Logger
+	}
+	type args struct {
+		input subs.FtcSubsCreationInput
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Create ftc member",
+			fields: fields{
+				db:     test.DBX,
+				logger: zaptest.NewLogger(t),
+			},
+			args: args{
+				input: p.FtcSubsCreationInput(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				logger: tt.fields.logger,
+			}
+			got, err := env.CreateFtcMember(tt.args.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
+
+func TestEnv_UpdateFtcMember(t *testing.T) {
+	p := test.NewPersona()
+	test.NewRepo().MustCreateMembership(p.Membership())
+	type fields struct {
+		db     *sqlx.DB
+		logger *zap.Logger
+	}
+	type args struct {
+		compoundID string
+		input      subs.FtcSubsUpdateInput
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Update ftc member",
+			fields: fields{
+				db:     test.DBX,
+				logger: zaptest.NewLogger(t),
+			},
+			args: args{
+				compoundID: p.FtcID,
+				input:      p.FtcSubsUpdateInput(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				logger: tt.fields.logger,
+			}
+			got, err := env.UpdateFtcMember(tt.args.compoundID, tt.args.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
+
+func TestEnv_DeleteFtcMember(t *testing.T) {
+	p := test.NewPersona()
+	test.NewRepo().MustCreateMembership(p.Membership())
+
+	type fields struct {
+		db     *sqlx.DB
+		logger *zap.Logger
+	}
+	type args struct {
+		compoundID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Delete ftc member",
+			fields: fields{
+				db:     test.DBX,
+				logger: zaptest.NewLogger(t),
+			},
+			args: args{
+				compoundID: p.FtcID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := Env{
+				db:     tt.fields.db,
+				logger: tt.fields.logger,
+			}
+			got, err := env.DeleteFtcMember(tt.args.compoundID)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
+		})
+	}
+}
