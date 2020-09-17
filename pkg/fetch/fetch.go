ff2eb47ad91929f1ed858e6c3dff479ac7acab42
weiguo.ni <neefrankie@gmail.com>
2020-09-17 17:08:53 +0800

    refactor: fetch

--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -8,17 +8,28 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 var httpClient = &http.Client{}
 
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
+func (a BasicAuth) IsZero() bool {
+	return a.Username == "" || a.Password == ""
+}
+
 type Fetch struct {
-	method string
-	url    string
-	Query  url.Values
-	body   io.Reader
-	Header http.Header
-	Errors []error
+	method    string
+	url       string
+	Query     url.Values
+	body      io.Reader
+	Header    http.Header
+	Errors    []error
+	basicAuth BasicAuth
 }
 
 func New() *Fetch {
@@ -73,19 +84,48 @@ func (f *Fetch) SetParamMap(kv map[string]string) *Fetch {
 	return f
 }
 
-func (f *Fetch) AddParam(key, value string) *Fetch {
+func (f *Fetch) AddQueryParam(key, value string) *Fetch {
 	f.Query.Add(key, value)
 	return f
 }
 
-func (f *Fetch) SetAuth(key string) *Fetch {
+func (f *Fetch) SetQuery(q url.Values) *Fetch {
+	f.Query = q
+
+	return f
+}
+
+func (f *Fetch) SetBearerAuth(key string) *Fetch {
 	f.Header.Set("Authorization", "Bearer "+key)
+
+	return f
+}
+
+func (f *Fetch) SetBasicAuth(username, password string) *Fetch {
+	f.basicAuth = BasicAuth{
+		Username: strings.TrimSpace(username),
+		Password: strings.TrimSpace(password),
+	}
 
 	return f
 }
 
 func (f *Fetch) AcceptLang(v string) *Fetch {
 	f.Header.Set("Accept-Language", v)
+
+	return f
+}
+
+func (f *Fetch) SetHeader(k, v string) *Fetch {
+	f.Header.Set(k, v)
+
+	return f
+}
+
+func (f *Fetch) SetHeaderMap(h map[string]string) *Fetch {
+	for k, v := range h {
+		f.Header.Set(k, v)
+	}
 
 	return f
 }
@@ -126,6 +166,10 @@ func (f *Fetch) End() (*http.Response, []error) {
 
 	req.Header = f.Header
 
+	if !f.basicAuth.IsZero() {
+		req.SetBasicAuth(f.basicAuth.Username, f.basicAuth.Password)
+	}
+
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		f.Errors = append(f.Errors, err)
@@ -135,7 +179,7 @@ func (f *Fetch) End() (*http.Response, []error) {
 	return resp, nil
 }
 
-func (f *Fetch) EndRaw() (*http.Response, []byte, []error) {
+func (f *Fetch) EndBytes() (*http.Response, []byte, []error) {
 	resp, errs := f.End()
 	if errs != nil {
 		return resp, nil, f.Errors
