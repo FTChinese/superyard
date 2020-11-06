c65c23cb2ce5890b83be00b9af9afad73cb04bcb
weiguo.ni <neefrankie@gmail.com>
2020-11-06 17:30:35 +0800

    rename

--- internal/repository/subsapi/ali_wx.go
+++ internal/repository/subsapi/ali_wx.go
@@ -3,13 +3,12 @@ package subsapi
 import (
 	"fmt"
 	"github.com/FTChinese/superyard/pkg/fetch"
-	"github.com/FTChinese/superyard/pkg/subs"
 	"log"
 	"net/http"
 )
 
-func (c Client) QueryOrder(order subs.Order) (*http.Response, error) {
-	url := fmt.Sprintf("%s/orders/%s", c.baseURL, order.ID)
+func (c Client) ConfirmOrder(orderID string) (*http.Response, error) {
+	url := fmt.Sprintf("%s/orders/%s", c.baseURL, orderID)
 
 	log.Printf("Query order payment result at %s", url)
 
