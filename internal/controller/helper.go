ded25dbb502cec766efae89fc0e3780a6f52f662
weiguo.ni <neefrankie@gmail.com>
2020-11-26 14:57:54 +0800

    feat: add schema decoder

--- internal/controller/helper.go
+++ internal/controller/helper.go
@@ -1,9 +1,12 @@
 package controller
 
 import (
+	"github.com/gorilla/schema"
 	"strconv"
 )
 
 func ParseInt(str string) (int64, error) {
 	return strconv.ParseInt(str, 10, 0)
 }
+
+var decoder = schema.NewDecoder()
