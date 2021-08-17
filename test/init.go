29445f8d06f51c19f77c9f1c5743ad29a4307c71
weiguo.ni <neefrankie@gmail.com>
2021-08-17 15:45:45 +0800

    refactor:

--- test/init.go
+++ test/init.go
@@ -2,20 +2,17 @@ package test
 
 import (
 	"github.com/FTChinese/superyard/faker"
-	"github.com/FTChinese/superyard/pkg/config"
 	"github.com/FTChinese/superyard/pkg/db"
-	"github.com/jmoiron/sqlx"
 	"math/rand"
 	"time"
 )
 
-var DBX *sqlx.DB
+var DBX db.ReadWriteMyDBs
 var Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-var CFG = config.Config{Debug: true}
 
 func init() {
 
 	faker.MustConfigViper()
 
-	DBX = db.MustNewDB(CFG.MustGetDBConn(""))
+	DBX = db.MustNewMyDBs(false)
 }
