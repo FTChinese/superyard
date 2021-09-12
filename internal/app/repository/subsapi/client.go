9dc5b60db08f2ae18ea13c14c2508357c713db93
weiguo.ni <neefrankie@gmail.com>
2021-09-12 22:55:32 +0800

    feat: use subs api to create price

--- internal/app/repository/subsapi/client.go
+++ internal/app/repository/subsapi/client.go
@@ -4,6 +4,26 @@ import (
 	"github.com/FTChinese/superyard/pkg/config"
 )
 
+const (
+	pathPaywall        = "/paywall"
+	pathRefreshPaywall = pathPaywall + "/__refresh"
+	pathPaywallPrices  = pathPaywall + "/active/prices"
+	pathProductPrices  = pathPaywall + "/prices"
+	pathPriceDiscounts = pathPaywall + "/discounts"
+)
+
+func pathPricesOfProduct(id string) string {
+	return pathProductPrices + "?product_id=" + id
+}
+
+func pathPriceOf(id string) string {
+	return pathProductPrices + "/" + id
+}
+
+func pathDiscountOf(id string) string {
+	return pathPriceDiscounts + "/" + id
+}
+
 type Client struct {
 	key     string
 	baseURL string
@@ -13,6 +33,6 @@ func NewClient(prod bool) Client {
 
 	return Client{
 		key:     config.MustSubsAPIKey().Pick(prod),
-		baseURL: config.MustSubsAPIv2BaseURL().Pick(prod),
+		baseURL: config.MustSubsAPISandboxBaseURL().Pick(prod),
 	}
 }
