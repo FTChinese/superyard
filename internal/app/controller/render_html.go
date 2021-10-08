0c3462f48699f163d1f3308caa955e818462060e
weiguo.ni <neefrankie@gmail.com>
2021-10-08 18:56:09 +0800

    refactor: use html template pkg

--- internal/app/controller/render_html.go
+++ internal/app/controller/render_html.go
@@ -1,9 +1,15 @@
 package controller
 
 import (
+	"github.com/FTChinese/go-rest/chrono"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"time"
 )
+
+type HomeCtx struct {
+	Year int
+}
 
 func Home(c echo.Context) error {
 	c.Response().Header().Add("Cache-Control", "no-cache")
@@ -11,5 +17,7 @@ func Home(c echo.Context) error {
 	c.Response().Header().Add("Cache-Control", "must-revalidate")
 	c.Response().Header().Add("Pragma", "no-cache")
 
-	return c.Render(http.StatusOK, "home", nil)
+	return c.Render(http.StatusOK, "home", HomeCtx{
+		Year: time.Now().In(chrono.TZShanghai).Year(),
+	})
 }
