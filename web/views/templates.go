e89754390bc086a1c1dc7e913dee14f0e83333d8
weiguo.ni <neefrankie@gmail.com>
2021-10-15 13:37:11 +0800

    v0.11

--- web/views/templates.go
+++ web/views/templates.go
@@ -4,36 +4,38 @@ package views
 // Do not touch it.
 
 const home = `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
+<!DOCTYPE html><html lang="en"><head>
     <base href="/">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="apple-touch-icon" sizes="180x180" href="http://interactive.ftchinese.com/favicons/apple-touch-icon-180x180.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="http://interactive.ftchinese.com/favicons/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="http://interactive.ftchinese.com/favicons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="http://interactive.ftchinese.com/favicons/apple-touch-icon-76x76.png">
-    <link href="http://interactive.ftchinese.com/favicons/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://interactive.ftchinese.com/favicons/apple-touch-icon-180x180.png"><link rel="apple-touch-icon" sizes="152x152" href="https://interactive.ftchinese.com/favicons/apple-touch-icon-152x152.png"><link rel="apple-touch-icon" sizes="120x120" href="https://interactive.ftchinese.com/favicons/apple-touch-icon-120x120.png"><link rel="apple-touch-icon" sizes="76x76" href="https://interactive.ftchinese.com/favicons/apple-touch-icon-76x76.png">
+    <link href="https://interactive.ftchinese.com/favicons/favicon.ico" type="image/x-icon" rel="icon">
     <title>Superyard</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css" rel="stylesheet">
-    <link onload="this.media='all'" media="print" href="/static/superyard/styles.f1b6e97faeb56a2bc280.css" rel="stylesheet"/>
-<link href="/static/superyard/styles.f1b6e97faeb56a2bc280.css" rel="stylesheet"/>
-</head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.2/css/bootstrap.min.css" rel="stylesheet">
+<style>body{font-family:Helvetica Neue,Lucida Grande,Verdana,PingFang SC,PingFang TC,Hiragino Sans GB,Heiti SC,Heiti TC,WenQuanYi Micro Hei,Microsoft YaHei,Microsoft JhengHei,STHeiti,sans-serif;background-color:#fff1e5;}.o-footer{margin-top:40px;border-top:10px solid;line-height:20px;font-size:16px;}.o-footer--theme-dark{color:#fff;background-color:#33302e;border-color:#0a5e66;}.o-footer--theme-dark .o-footer__copyright{color:#b3a9a0;}@media print{.o-footer{display:none;}}</style><link rel="stylesheet" href="/static/superyard/styles.f1b6e97faeb56a2bc280.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="/static/superyard/styles.f1b6e97faeb56a2bc280.css"></noscript></head>
 
 <body>
     <app-root></app-root>
-    <script type="module" src="/static/superyard/runtime-es2015.63aa144b546cc3463235.js"></script>
-<script defer nomodule src="/static/superyard/runtime-es5.63aa144b546cc3463235.js"></script>
-<script defer nomodule src="/static/superyard/polyfills-es5.d1b3a905fb403e312835.js"></script>
-<script type="module" src="/static/superyard/polyfills-es2015.1ed7845cd38328d4cb06.js"></script>
-<script type="module" src="/static/superyard/main-es2015.ed831fec7b9ee0f26b3a.js"></script>
-<script defer nomodule src="/static/superyard/main-es5.ed831fec7b9ee0f26b3a.js"></script>
-</body>
-</html>
-`
+    <footer class="o-footer o-footer--theme-dark">
+  <div class="container">
+    <div class="o-footer__copyright pt-3 pb-3">
+      <small>
+        © FT中文网 {{.Year}}.
+        <abbr title="Financial Times" aria-label="F T">FT</abbr> and ‘Financial Times’ are trademarks of The Financial
+        Times Ltd.
+      </small>
+    </div>
+
+    <div class="text-muted pb-2">
+      <small>Client v0.10.4. Server {{.ServerVersion}}</small>
+    </div>
+  </div>
+</footer>
+
+<script src="/static/superyard/runtime-es2015.63aa144b546cc3463235.js" type="module"></script><script src="/static/superyard/runtime-es5.63aa144b546cc3463235.js" nomodule="" defer=""></script><script src="/static/superyard/polyfills-es5.d1b3a905fb403e312835.js" nomodule="" defer=""></script><script src="/static/superyard/polyfills-es2015.1ed7845cd38328d4cb06.js" type="module"></script><script src="/static/superyard/main-es2015.fbb89d811da286b10229.js" type="module"></script><script src="/static/superyard/main-es5.fbb89d811da286b10229.js" nomodule="" defer=""></script>
+
+</body></html>`
 
 var templates = map[string]string{
 
