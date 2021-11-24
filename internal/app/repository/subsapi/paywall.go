70ffd7557da10535432fadddb6337f839d391711
weiguo.ni <neefrankie@gmail.com>
2021-11-24 09:59:03 +0800

    refactor: price related features moved to separate file

--- internal/app/repository/subsapi/paywall.go
+++ internal/app/repository/subsapi/paywall.go
@@ -57,23 +57,13 @@ func (c Client) LoadPaywall() (*http.Response, error) {
 	return resp, nil
 }
 
-// CreatePrice creates a new price for a product.
-// Input:
-// createBy: string;
-// tier: string;
-// cycle: string;
-// description?: string;
-// liveMode: boolean;
-// nickname?: string;
-// price: number;
-// productId: string;
-func (c Client) CreatePrice(body io.Reader) (*http.Response, error) {
-	url := c.baseURL + pathProductPrices
+func (c Client) CreatePaywallBanner(body io.Reader) (*http.Response, error) {
+	url := c.baseURL + pathPaywallBanner
 
 	resp, errs := fetch.New().
 		Post(url).
 		SetBearerAuth(c.key).
-		Send(body).
+		StreamJSON(body).
 		End()
 
 	if errs != nil {
@@ -83,89 +73,13 @@ func (c Client) CreatePrice(body io.Reader) (*http.Response, error) {
 	return resp, nil
 }
 
-// ListPriceOfProduct loads all prices under a product.
-func (c Client) ListPriceOfProduct(productID string) (*http.Response, error) {
-	url := c.baseURL + pathPricesOfProduct(productID)
-
-	resp, errs := fetch.New().
-		Get(url).
-		SetBearerAuth(c.key).
-		End()
-
-	if errs != nil {
-		return nil, errs[0]
-	}
-
-	return resp, nil
-}
-
-// ActivatePrice by id and returned and activated FtcPrice.
-func (c Client) ActivatePrice(priceID string) (*http.Response, error) {
-	url := c.baseURL + pathPriceOf(priceID)
+func (c Client) CreatePaywallPromoBanner(body io.Reader) (*http.Response, error) {
+	url := c.baseURL + pathPaywallPromo
 
 	resp, errs := fetch.New().
 		Post(url).
 		SetBearerAuth(c.key).
-		End()
-
-	if errs != nil {
-		return nil, errs[0]
-	}
-
-	return resp, nil
-}
-
-// RefreshPriceDiscounts update a price's discount list.
-// Returns the updated FtcPrice.
-func (c Client) RefreshPriceDiscounts(priceID string) (*http.Response, error) {
-	url := c.baseURL + pathPriceOf(priceID)
-
-	resp, errs := fetch.New().
-		Patch(url).
-		SetBearerAuth(c.key).
-		End()
-
-	if errs != nil {
-		return nil, errs[0]
-	}
-
-	return resp, nil
-}
-
-// CreateDiscount for a price and returns the created one.
-// Input:
-// createdBy: string;
-// description?: string;
-// kind: introductory | promotion | retention | win_back
-// percent: number;
-// startUtc: string;
-// endUtc: string;
-// priceOff: number;
-// priceId: string;
-// recurring: boolean;
-func (c Client) CreateDiscount(body io.Reader) (*http.Response, error) {
-	url := c.baseURL + pathPriceDiscounts
-
-	resp, errs := fetch.New().
-		Post(url).
-		SetBearerAuth(c.key).
-		Send(body).End()
-
-	if errs != nil {
-		return nil, errs[0]
-	}
-
-	return resp, nil
-}
-
-// RemoveDiscount from a ftc price.
-// Returns FtcPrice
-func (c Client) RemoveDiscount(id string) (*http.Response, error) {
-	url := c.baseURL + pathDiscountOf(id)
-
-	resp, errs := fetch.New().
-		Delete(url).
-		SetBearerAuth(c.key).
+		StreamJSON(body).
 		End()
 
 	if errs != nil {
---
+++ internal/app/repository/subsapi/paywall_price.go
@@ -0,0 +1,125 @@
+package subsapi
+
+import (
+	"github.com/FTChinese/superyard/pkg/fetch"
+	"io"
+	"net/http"
+)
+
+// CreatePrice creates a new price for a product.
+// Input:
+// createBy: string;
+// tier: string;
+// cycle: string;
+// description?: string;
+// liveMode: boolean;
+// nickname?: string;
+// price: number;
+// productId: string;
+func (c Client) CreatePrice(body io.Reader) (*http.Response, error) {
+	url := c.baseURL + pathPrices
+
+	resp, errs := fetch.New().
+		Post(url).
+		SetBearerAuth(c.key).
+		Send(body).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+// ListPriceOfProduct loads all prices under a product.
+func (c Client) ListPriceOfProduct(productID string) (*http.Response, error) {
+	url := c.baseURL + pathPricesOfProduct(productID)
+
+	resp, errs := fetch.New().
+		Get(url).
+		SetBearerAuth(c.key).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+// ActivatePrice by id and returned and activated FtcPrice.
+func (c Client) ActivatePrice(priceID string) (*http.Response, error) {
+	url := c.baseURL + pathPriceOf(priceID)
+
+	resp, errs := fetch.New().
+		Post(url).
+		SetBearerAuth(c.key).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+// RefreshPriceDiscounts update a price's discount list.
+// Returns the updated FtcPrice.
+func (c Client) RefreshPriceDiscounts(priceID string) (*http.Response, error) {
+	url := c.baseURL + pathPriceOf(priceID)
+
+	resp, errs := fetch.New().
+		Patch(url).
+		SetBearerAuth(c.key).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+// CreateDiscount for a price and returns the created one.
+// Input:
+// createdBy: string;
+// description?: string;
+// kind: introductory | promotion | retention | win_back
+// percent: number;
+// startUtc: string;
+// endUtc: string;
+// priceOff: number;
+// priceId: string;
+// recurring: boolean;
+func (c Client) CreateDiscount(body io.Reader) (*http.Response, error) {
+	url := c.baseURL + pathPriceDiscounts
+
+	resp, errs := fetch.New().
+		Post(url).
+		SetBearerAuth(c.key).
+		Send(body).End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+// RemoveDiscount from a ftc price.
+// Returns FtcPrice
+func (c Client) RemoveDiscount(id string) (*http.Response, error) {
+	url := c.baseURL + pathDiscountOf(id)
+
+	resp, errs := fetch.New().
+		Delete(url).
+		SetBearerAuth(c.key).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
