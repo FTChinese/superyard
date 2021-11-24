b6bf0f33377516cc62c0acd28880a6913662bccf
weiguo.ni <neefrankie@gmail.com>
2021-11-24 10:03:56 +0800

    refactor: paywall uses api

---
+++ internal/app/controller/paywall_banner.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/superyard/pkg/fetch"
+	"github.com/labstack/echo/v4"
+)
+
+// CreateBanner creates a single unique banner.
+// Request: paywall.BannerInput
+// { heading: string;
+//   coverUrl?: string;
+//   subHeading?: string;
+//   content?; string;
+// }
+// Response: paywall.Banner
+func (router PaywallRouter) CreateBanner(c echo.Context) error {
+	//claims := getPassportClaims(c)
+
+	live := getParamLive(c)
+
+	defer c.Request().Body.Close()
+
+	resp, err := router.apiClients.
+		Select(live).
+		CreatePaywallBanner(c.Request().Body)
+
+	if err != nil {
+		return render.NewBadRequest(err.Error())
+	}
+
+	return c.Stream(resp.StatusCode, fetch.ContentJSON, resp.Body)
+}
+
+// CreatePromoBanner creates a new promo and apply it to current banner.
+// Request: paywall.PromoInput
+// Response: paywall.Promo
+func (router PaywallRouter) CreatePromoBanner(c echo.Context) error {
+	//claims := getPassportClaims(c)
+
+	live := getParamLive(c)
+
+	defer c.Request().Body.Close()
+
+	resp, err := router.apiClients.
+		Select(live).
+		CreatePaywallPromoBanner(c.Request().Body)
+
+	if err != nil {
+		return render.NewBadRequest(err.Error())
+	}
+
+	return c.Stream(resp.StatusCode, fetch.ContentJSON, resp.Body)
+}
+
+func (router PaywallRouter) DropPromoBanner(c echo.Context) error {
+	return nil
+}
---
+++ internal/app/controller/paywall_router.go
@@ -0,0 +1,94 @@
+package controller
+
+import (
+	"github.com/FTChinese/go-rest/render"
+	"github.com/FTChinese/superyard/internal/app/repository/subsapi"
+	"github.com/FTChinese/superyard/pkg/fetch"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+	"strconv"
+)
+
+type PaywallRouter struct {
+	apiClients subsapi.APIClients
+	logger     *zap.Logger
+}
+
+func NewPaywallRouter(clients subsapi.APIClients, logger *zap.Logger) PaywallRouter {
+	return PaywallRouter{
+		apiClients: clients,
+		logger:     logger,
+	}
+}
+
+// LoadPaywall gets a paywall's banner, optional promo and a list of products.
+func (router PaywallRouter) LoadPaywall(c echo.Context) error {
+
+	liveMode := getParamLive(c)
+
+	resp, err := router.apiClients.Select(liveMode).LoadPaywall()
+
+	if err != nil {
+		return render.NewBadRequest(err.Error())
+	}
+
+	return c.Stream(resp.StatusCode, fetch.ContentJSON, resp.Body)
+}
+
+// RefreshFtcPaywall bust cache of paywall, either in live mode or not.
+// When busting cache, we have to clean caches for
+// * Live version
+// * Sandbox version
+// * V3
+// Plus Stripe prices, we have a total of 6 endpoints to hit.
+func (router PaywallRouter) RefreshFtcPaywall(c echo.Context) error {
+	defer router.logger.Sync()
+	sugar := router.logger.Sugar()
+
+	liveMode := getParamLive(c)
+
+	resp, err := router.apiClients.Select(liveMode).RefreshFtcPaywall()
+	if err != nil {
+		sugar.Error(err)
+		return render.NewBadRequest(err.Error())
+	}
+
+	// Also bust cache of v3.
+	if liveMode {
+		go func() {
+			sugar.Infof("Paywall cach bust v3")
+			_, err := router.apiClients.V3.RefreshFtcPaywall()
+			if err != nil {
+				sugar.Error(err)
+			}
+		}()
+	}
+
+	return c.Stream(resp.StatusCode, fetch.ContentJSON, resp.Body)
+}
+
+func (router PaywallRouter) RefreshStripePrices(c echo.Context) error {
+	defer router.logger.Sync()
+	sugar := router.logger.Sugar()
+
+	liveMode, _ := strconv.ParseBool(c.QueryParam("live"))
+
+	resp, err := router.apiClients.Select(liveMode).RefreshStripePrices()
+	if err != nil {
+		sugar.Error(err)
+		return render.NewBadRequest(err.Error())
+	}
+
+	// Also bust cache of v3.
+	if liveMode {
+		go func() {
+			sugar.Infof("Paywall cach bust v3")
+			_, err := router.apiClients.V3.RefreshStripePrices()
+			if err != nil {
+				sugar.Error(err)
+			}
+		}()
+	}
+
+	return c.Stream(resp.StatusCode, fetch.ContentJSON, resp.Body)
+}
