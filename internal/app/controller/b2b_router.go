cc82fb90300702f4877e9c453c45d3fed4baf804
weiguo.ni <neefrankie@gmail.com>
2022-05-20 11:20:56 +0800

    refactor: rename b2bapi to ftaapi

--- internal/app/controller/b2b_router.go
+++ internal/app/controller/b2b_router.go
@@ -1,13 +1,13 @@
 package controller
 
-import "github.com/FTChinese/superyard/internal/app/repository/b2bapi"
+import "github.com/FTChinese/superyard/internal/app/repository/ftaapi"
 
 type B2BRouter struct {
-	apiClient b2bapi.B2BClient
+	apiClient ftaapi.FtaClient
 }
 
 func NewB2BRouter(prod bool) B2BRouter {
 	return B2BRouter{
-		apiClient: b2bapi.NewClient(prod),
+		apiClient: ftaapi.NewClient(prod),
 	}
 }
