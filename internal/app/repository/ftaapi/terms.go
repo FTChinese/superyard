084c8b4b46384390b82c7b2d9f334250b2117a9e
weiguo.ni <neefrankie@gmail.com>
2022-05-20 11:21:11 +0800

    feat: refresh legal terms page

---
+++ internal/app/repository/ftaapi/terms.go
@@ -0,0 +1,25 @@
+package ftaapi
+
+import (
+	"fmt"
+	"github.com/FTChinese/superyard/pkg/fetch"
+)
+
+func (c FtaClient) RefreshTermsDoc(id string) error {
+	url := fetch.NewURLBuilder(c.baseURL).
+		AddPath(baseTerms).
+		AddPath(id).
+		AddQueryBool("refresh", true).
+		String()
+
+	resp, errs := fetch.New().Get(url).End()
+	if errs != nil {
+		return errs[0]
+	}
+
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("%d: %s", resp.StatusCode, resp.Status)
+	}
+
+	return nil
+}
