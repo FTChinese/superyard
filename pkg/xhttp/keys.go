6b9b5002889794e8e483cd42b9097b392bbfc413
weiguo.ni <neefrankie@gmail.com>
2022-07-26 17:45:27 +0800

    refactor: client header

--- pkg/xhttp/keys.go
+++ pkg/xhttp/keys.go
@@ -5,4 +5,9 @@ const (
 	XUserID      = "X-User-Id"
 	XUnionID     = "X-Union-Id"
 	XStaffName   = "X-Staff-Name"
+
+	keyClientType    = "X-Client-Type"
+	keyClientVersion = "X-Client-Version"
+	keyUserIP        = "X-User-Ip"
+	keyUserAgent     = "X-User-Agent"
 )
