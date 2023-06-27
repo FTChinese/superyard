9a6202ad4b248ae01ea437cd031e340929737d79
weiguo.ni <neefrankie@gmail.com>
2023-06-27 17:29:19 +0800

    removed MockMySQL

--- pkg/db/mock.go
+++ pkg/db/mock.go
@@ -4,11 +4,6 @@ package db
 
 import "github.com/FTChinese/superyard/faker"
 
-func MockMySQL() ReadWriteMyDBs {
-	faker.MustSetupViper()
-	return MustNewMyDBs(false)
-}
-
 func MockGormSQL() MultiGormDBs {
 	faker.MustSetupViper()
 	return MustNewMultiGormDBs(false)
