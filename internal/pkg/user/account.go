d4849365dcf7e4946a0d755f90e81eca65375b19
weiguo.ni <neefrankie@gmail.com>
2023-07-06 15:36:48 +0800

    removed sql clause

--- internal/pkg/user/account.go
+++ internal/pkg/user/account.go
@@ -1,16 +1,7 @@
 package user
 
 import (
-	"github.com/FTChinese/go-rest/chrono"
 	"github.com/guregu/null"
-)
-
-var StmtAccountCols = []string{"id", "username", "email", "fullname"}
-
-const (
-	StmtAuthBy         = "username = ? AND password = MD5(?)"
-	StmtVerifyPass     = "id = ? AND password = MD5(?)"
-	StmtAccountByEmail = "email = ?"
 )
 
 type Account struct {
@@ -35,25 +26,7 @@ func (a Account) NormalizeName() string {
 
 type Profile struct {
 	Account
-	CreatedAt   chrono.Time `json:"createdAt" gorm:"column:creatdate"`
-	LastLoginAt chrono.Time `json:"lastLoginAt" gorm:"column:lastlogin"`
+	CreatedAt   string      `json:"createdAt" gorm:"column:creatdate"`
+	LastLoginAt string      `json:"lastLoginAt" gorm:"column:lastlogin"`
 	LastLoginIP null.String `json:"lastLoginIp" gorm:"column:last_login_ip"`
 }
-
-// StmtListAccounts retrieves a list of accounts.
-// Restricted to admin privilege.
-const StmtListAccounts = `
-SELECT s.staff_id 		AS staff_id,
-	s.user_name 		AS user_name,
-	IFNULL(s.email, '') AS email,
-	s.is_active 		AS is_active,
-	s.display_name 		AS display_name,
-	s.department 		AS department,
-	s.group_memberships AS group_memberships
-FROM backyard.staff AS s
-ORDER BY s.user_name ASC
-LIMIT ? OFFSET ?`
-
-const StmtCountStaff = `
-SELECT COUNT(*)
-FROM backyard.staff`
