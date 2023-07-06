0b6b5c7ebb8daed732ec4f5c7813953091b8aaeb
weiguo.ni <neefrankie@gmail.com>
2023-07-06 17:33:23 +0800

    fix: gorm

--- pkg/reader/profile.go
+++ pkg/reader/profile.go
@@ -9,12 +9,16 @@ import (
 // WxProfile show wx-only account
 type WxProfile struct {
 	UnionID   string      `json:"unionId" db:"union_id" gorm:"primaryKey"`
-	Nickname  null.String `json:"nickname" db:"nickname"`
-	AvatarURL null.String `json:"avatarUrl" db:"avatar_url"`
-	Gender    enum.Gender `json:"gender" db:"gender"`
-	Country   null.String `json:"country" db:"country"`
-	Province  null.String `json:"province" db:"province"`
-	City      null.String `json:"city" db:"city"`
-	CreatedAt chrono.Time `json:"createdAt" db:"created_at"`
-	UpdatedAt chrono.Time `json:"updatedAt" db:"updated_at"`
+	Nickname  null.String `json:"nickname" gorm:"column:nickname"`
+	AvatarURL null.String `json:"avatarUrl" gorm:"column:avatar_url"`
+	Gender    enum.Gender `json:"gender" gorm:"column:gender"`
+	Country   null.String `json:"country" gorm:"column:country"`
+	Province  null.String `json:"province" gorm:"column:province"`
+	City      null.String `json:"city" gorm:"column:city"`
+	CreatedAt chrono.Time `json:"createdAt" gorm:"column:created_utc"`
+	UpdatedAt chrono.Time `json:"updatedAt" gorm:"column:updated_utc"`
+}
+
+func (w WxProfile) TableName() string {
+	return "user_db.wechat_userinfo"
 }
