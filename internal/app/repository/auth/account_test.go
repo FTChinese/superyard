75ea5804467c4b2fecb9c20746ec406d733ac502
weiguo.ni <neefrankie@gmail.com>
2023-07-06 15:37:13 +0800

    test

--- internal/app/repository/auth/account_test.go
+++ internal/app/repository/auth/account_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/FTChinese/go-rest/chrono"
@@ -82,6 +83,141 @@ func TestEnv_createdAccount(t *testing.T) {
 				return
 			}
 
+			t.Logf("%v", got)
+		})
+	}
+}
+
+func TestEnv_AccountByID(t *testing.T) {
+	env := NewEnv(db.MockGormSQL())
+
+	a, _ := env.createdAccount(mockNewAccount())
+
+	type args struct {
+		id int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    user.Account
+		wantErr bool
+	}{
+		{
+			name: "account by id",
+			args: args{
+				id: a.ID,
+			},
+			want:    a.Account,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := env.AccountByID(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Env.AccountByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Env.AccountByID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEnv_SetEmail(t *testing.T) {
+	env := NewEnv(db.MockGormSQL())
+
+	a, _ := env.createdAccount(mockNewAccount())
+
+	type args struct {
+		a user.Account
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "set email",
+			args: args{
+				a: a.Account,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if err := env.SetEmail(tt.args.a); (err != nil) != tt.wantErr {
+				t.Errorf("Env.SetEmail() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestEnv_UpdateDisplayName(t *testing.T) {
+	env := NewEnv(db.MockGormSQL())
+
+	a, _ := env.createdAccount(mockNewAccount())
+
+	type args struct {
+		a user.Account
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "updated display name",
+			args: args{
+				a: a.Account,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if err := env.UpdateDisplayName(tt.args.a); (err != nil) != tt.wantErr {
+				t.Errorf("Env.UpdateDisplayName() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestEnv_RetrieveProfile(t *testing.T) {
+	env := NewEnv(db.MockGormSQL())
+
+	a, _ := env.createdAccount(mockNewAccount())
+
+	type args struct {
+		id int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    user.Profile
+		wantErr bool
+	}{
+		{
+			name: "retrieve profile",
+			args: args{
+				id: a.ID,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := env.RetrieveProfile(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Env.RetrieveProfile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 			t.Logf("%v", got)
 		})
 	}
